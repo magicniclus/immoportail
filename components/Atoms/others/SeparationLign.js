ea8b6b9cc9baa40652532d8cd53b5d186e7a6afb
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-19 15:46:45 +0100

    Create SeparationLign.js

---
+++ components/Atoms/others/SeparationLign.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SeparationLign = (props) => {
+    const color = props.color
+
+    const classDesign = {
+        blue:"w-20 h-2 bg-blue rounded-full",
+        white:"w-20 h-2 bg-white rounded-full",
+        yellow:"w-20 h-2 bg-yellow rounded-full",
+        purple:"w-20 h-2 bg-purple rounded-full"
+    }
+
+    return (
+        <div className={color !== undefined ? classDesign[color] : classDesign.blue}>
+            
+        </div>
+    );
+};
+
+export default SeparationLign;
