f133d1e90a80472611c26d2d0da3a9d4bf23a32e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-21 13:38:16 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import Header from '../components/Organisms/Header'
-import InputWhithLogo from '../components/Atoms/inputs/inputWhithLogo'
-import ButtonPrimary from '../components/Atoms/buttons/ButtonPrimary'
-import TitlePrimary from '../components/Atoms/titles/TitlePrimary'
-import TitleSecondary from '../components/Atoms/titles/TitleSecondary'
-import Text from '../components/Atoms/texts/Text'
-import SeparationLign from '../components/Atoms/others/SeparationLign'
+import EstimationBanner from '../components/Organisms/EstimationBanner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,17 +16,7 @@ export default function Home() {
         <Header />
       </header>
       <main>
-          <div className="bg-purple px-14 py-8" style={{width: "717px"}}>
-            <TitlePrimary title="Estimez votre bien en ligne" titleWithColor="gratuitement et en 2 minutes" />
-            <SeparationLign color="white" />
-            <TitleSecondary title="Adress du bien à estimer" color="primary" />
-            <InputWhithLogo logoType="/Vectorlocalization.png" placeholder="Saisir une adresse"/>
-            <ButtonPrimary title="Estimer"  version="secondary"/>
-            <Text textSize="normal" color="white">
-              Nos estimations sont réalisées grâce aux caractéristiques de 
-              votre bien immobilier et du marché local en temps réel
-            </Text>
-          </div>
+        <EstimationBanner />
       </main>
     </>
   )
