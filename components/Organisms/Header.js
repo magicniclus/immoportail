d6dc53f7d6975fb2717a1aca06107f41eb6a3fa4
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-21 08:52:51 +0100

    Update Header.js

--- components/Organisms/Header.js
+++ components/Organisms/Header.js
@@ -1,14 +1,13 @@
 import Image from 'next/image';
 import React from 'react';
+import Logo from '../Atoms/logos/Logo';
 import SetOfMenuButtons from '../Molecules/SetOfMenuButtons';
 
 const Header = () => {
     return (
         <div className='w-screen bg-white h-28 drop-shadow-md flex items-center place-content-between px-5 md:px-40'>
             {/* Left container */}
-            <div className="cursor-pointer h-7 relative flex items-center w-40 md:w-56">
-                <Image priority src="/Portailimmologo.png" layout='fill' objectFit='contain' objectPosition='left'  />
-            </div>
+            <Logo />
             {/* Right container */}
             <SetOfMenuButtons />
         </div>
