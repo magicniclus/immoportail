2b5ab7d19759a6eca7c775478534b469fb32a4c8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-21 13:38:10 +0100

    Update TitleSecondary.js

--- components/Atoms/titles/TitleSecondary.js
+++ components/Atoms/titles/TitleSecondary.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 const TitleSecondary = (props) => {
-    const title = props.title;
-    const titleWithColor = props.titleWithColor
+    const text = props.text;
+    const textWithColor = props.textWithColor
     const color = props.color;
 
     const classDesign = {
-        primary: "text-white text-base md:text-2xl",
-        secondary: "text-purple text-base md:text-2xl"
+        white: "text-white text-base md:text-2xl",
+        purple: "text-purple text-base md:text-2xl"
     }
 
     return (
         <>
             {
-                titleWithColor !== undefined && titleWithColor !== null ? 
-                <h2 className={color !== undefined && color !== null ?  classDesign[color] : classDesign.primary}>{title}&nbsp;<span className='text-blue'>{titleWithColor}</span></h2>:
-                <h2 className={color !== undefined && color !== null ?  classDesign[color] : classDesign.primary}>{title}</h2>
+                textWithColor !== undefined && textWithColor !== null ? 
+                <h2 className={color !== undefined && color !== null ?  classDesign[color] : classDesign.white}>{text}&nbsp;<span className='text-blue'>{textWithColor}</span></h2>:
+                <h2 className={color !== undefined && color !== null ?  classDesign[color] : classDesign.white}>{text}</h2>
             }
         </>
     );
