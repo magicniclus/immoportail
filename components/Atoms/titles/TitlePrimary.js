b6c6f939b241d033c1a1e3c8baf3471149eb7128
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-21 15:34:05 +0100

    Update TitlePrimary.js

--- components/Atoms/titles/TitlePrimary.js
+++ components/Atoms/titles/TitlePrimary.js
@@ -7,8 +7,8 @@ const TitlePrimary = (props) => {
         <>
             {
                 textWithColor !== undefined && textWithColor !== null ? 
-                <h1 className='text-white text-2xl md:text-4xl font-bold md:w-3/5'>{text}&nbsp;<span className='text-blue'>{textWithColor}</span></h1>:
-                <h1 className='text-white text-2xl md:text-4xl font-bold md:w-3/5'>{text}</h1>
+                <h1 className='text-white text-2xl md:text-4xl font-bold md:w-10/12'>{text}&nbsp;<span className='text-blue'>{textWithColor}</span></h1>:
+                <h1 className='text-white text-2xl md:text-4xl font-bold md:w-10/12'>{text}</h1>
             }
         </>
     );
