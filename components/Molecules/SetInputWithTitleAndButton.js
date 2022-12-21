50c16896d00682d14184ea58d3ad9674321c2438
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-21 13:38:12 +0100

    Create SetInputWithTitleAndButton.js

---
+++ components/Molecules/SetInputWithTitleAndButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TitleSecondary from "../Atoms/titles/TitleSecondary"
+import InputWithLogo from '../Atoms/inputs/InputWithLogo';
+import ButtonPrimary from '../Atoms/buttons/ButtonPrimary';
+
+const SetInputWithTitleAndButton = () => {
+    return (
+        <div className='bg-white w-4/6 py-5 px-7 my-4 rounded-lg'>
+            <TitleSecondary text="Adresse du bien à estimer" color="purple" />
+            <InputWithLogo image="/Vectorlocalization.png" marginY="my-4"/>
+            <ButtonPrimary text="Estimer" version="primary" />
+        </div>
+    );
+};
+
+export default SetInputWithTitleAndButton;
