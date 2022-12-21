b4df6ac6007b0676c02f719c2e2c365a88280218
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-21 15:34:02 +0100

    Create Point.js

---
+++ components/Atoms/others/Point.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+
+const Point = (props) => {
+    const active = props.active
+    useEffect(()=>{
+        console.log(active);
+    }, [])
+    return (
+        <div className={active === true ? 'cursor-pointer h-4 w-4 bg-white opacity-100 rounded-full':'h-4 w-4 bg-white opacity-40 rounded-full'}></div>
+    );
+};
+
+export default Point;
