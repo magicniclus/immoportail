a7a1df72c318cebf0f2e0cbb4a2e5f47b50b4d06
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-21 15:34:10 +0100

    Update EstimationBanner.js

--- components/Organisms/EstimationBanner.js
+++ components/Organisms/EstimationBanner.js
@@ -3,12 +3,13 @@ import SeparationLign from '../Atoms/others/SeparationLign';
 import Text from '../Atoms/texts/Text';
 import SetInputWithTitleAndButton from '../Molecules/SetInputWithTitleAndButton';
 import TitlePrimary from '../Atoms/titles/TitlePrimary';
+import LightBox from '../Molecules/LightBox';
 
 const EstimationBanner = () => {
     return (
-        <div className='bg-purple px-5 md:px-40 py-2 md:py-16'>
+        <div className='bg-purple px-5 md:px-40 py-2 md:py-16 flex content-between relative'>
             {/* Right container */}
-            <div className="">
+            <div className=" lg:w-8/12">
                 <TitlePrimary text="Estimez votre bien en ligne" textWithColor="gratuitement en 2 minutes " />
                 <Text textSize="light" color="white" marginY="my-4">
                     Nos estimations sont réalisées grâce aux caractéristiques de 
@@ -19,7 +20,9 @@ const EstimationBanner = () => {
             </div>
 
             {/* Left  container */}
-            <div className=""></div>
+            <div className="hidden h-full bg-white lg:flex lg:w-4/12 absolute right-0 top-0">
+                <LightBox />
+            </div>
         </div>
     );
 };
