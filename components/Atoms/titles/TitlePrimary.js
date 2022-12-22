cbc6f57216a53e1fd7980908f4bbabc9c6aa0bc3
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 11:25:40 +0100

    Update TitlePrimary.js

--- components/Atoms/titles/TitlePrimary.js
+++ components/Atoms/titles/TitlePrimary.js
@@ -3,12 +3,19 @@ import React from 'react';
 const TitlePrimary = (props) => {
     const text = props.text;
     const textWithColor = props.textWithColor
+    const color = props.color
+
+    const classDesign = {
+        white:"text-white text-2xl md:text-4xl font-bold md:w-10/12",
+        purple:"text-purple text-2xl md:text-4xl font-bold md:w-10/12"
+    }
+
     return (
         <>
             {
                 textWithColor !== undefined && textWithColor !== null ? 
-                <h1 className='text-white text-2xl md:text-4xl font-bold md:w-10/12'>{text}&nbsp;<span className='text-blue'>{textWithColor}</span></h1>:
-                <h1 className='text-white text-2xl md:text-4xl font-bold md:w-10/12'>{text}</h1>
+                <h1 className={color !== undefined ? classDesign[color] : classDesign.white}>{text}&nbsp;<span className='text-blue'>{textWithColor}</span></h1>:
+                <h1 className={color !== undefined ? classDesign[color] : classDesign.white}>{text}</h1>
             }
         </>
     );
