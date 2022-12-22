05bbfd7c91f38e87ae4292c8ac788a107fd71209
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 19:58:27 +0100

    Update QualityBanner.js

--- components/Organisms/QualityBanner.js
+++ components/Organisms/QualityBanner.js
@@ -6,8 +6,8 @@ const QualityBanner = () => {
         <div className='w-full'>
             <div className="flex-col lg:flex-row flex justify-between py-10 md:py-16 items-center px-5 md:px-40">
                 <SetOfQuality image="Vectorcheck" title="Estimation" titleWithColor="en ligne" text="Estimez votre bien en quelques clic grâce à notre outil d’estimation 100% gratuit, fiable, basé sur les données officiel mis à jour en temps réel." />
-                <SetOfQuality image="Vectorcheck" title="Estimation" titleWithColor="en ligne" text="Estimez votre bien en quelques clic grâce à notre outil d’estimation 100% gratuit, fiable, basé sur les données officiel mis à jour en temps réel." />
-                <SetOfQuality image="Vectorcheck" title="Estimation" titleWithColor="en ligne" text="Estimez votre bien en quelques clic grâce à notre outil d’estimation 100% gratuit, fiable, basé sur les données officiel mis à jour en temps réel." />
+                <SetOfQuality image="Vectoruserpurple" title="Un unique" titleWithColor="conseiller" text="Estimez votre bien en quelques clic grâce à notre outil d’estimation 100% gratuit, fiable, basé sur les données officiel mis à jour en temps réel." />
+                <SetOfQuality image="Vectorsafe" title="Protection de" titleWithColor="vos données" text="Estimez votre bien en quelques clic grâce à notre outil d’estimation 100% gratuit, fiable, basé sur les données officiel mis à jour en temps réel." />
             </div>
         </div>
     );
