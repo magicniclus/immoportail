9bbfdac0facb352bc0f3e1bdf3494640e29d38cd
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 13:16:13 +0100

    Update EvaluationBanner.js

--- components/Organisms/EvaluationBanner.js
+++ components/Organisms/EvaluationBanner.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import SetOfAdvice from '../Molecules/SetOfAdvice';
 import SetTitleWithDescriptionAndMiddleSeparation from '../Molecules/SetTitleWithDescriptionAndMiddleSeparation';
 
 const EvaluationBanner = () => {
     return (
         <div className='w-full px-5 md:px-40 '>
             <SetTitleWithDescriptionAndMiddleSeparation title="Comment évaluer" titleWithColor="gratuitement son bien immobilier ?" colorTitle="purple" colorSeparation="yellow" />
+            <SetOfAdvice title="Notre conseil:" text="La plupart des propriétaire ont tendances à sur-évaluer leur bien. C’est pourquoi nous vous recommandons de vous renseigner valeur du marché actuel, de son évolution et surtout de vous entourer de professionnel compétant pour votre estimation." />
         </div>
     );
 };
