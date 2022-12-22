86e7249b198ac1a055a61225218fdfb500dbd6d4
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 11:38:36 +0100

    Update SeparationLign.js

--- components/Atoms/others/SeparationLign.js
+++ components/Atoms/others/SeparationLign.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const SeparationLign = (props) => {
     const color = props.color
+    const marginY = props.marginY
 
     const classDesign = {
-        blue:"w-20 h-2 bg-blue rounded-full",
-        white:"w-20 h-2 bg-white rounded-full",
-        yellow:"w-20 h-2 bg-yellow rounded-full",
-        purple:"w-20 h-2 bg-purple rounded-full"
+        blue:"w-20 h-2 bg-blue rounded-full"+" "+marginY,
+        white:"w-20 h-2 bg-white rounded-full"+" "+marginY,
+        yellow:"w-20 h-2 bg-yellow rounded-full"+" "+marginY,
+        purple:"w-20 h-2 bg-purple rounded-full"+" "+marginY
     }
 
     return (
