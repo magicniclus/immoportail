0eccd568266e6c60c9a8e558120fcfda71fcc110
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 10:22:34 +0100

    Update SetInputWithTitleAndButton.js

--- components/Molecules/SetInputWithTitleAndButton.js
+++ components/Molecules/SetInputWithTitleAndButton.js
@@ -7,7 +7,7 @@ const SetInputWithTitleAndButton = () => {
     return (
         <div className='bg-white py-5 px-7 my-4 rounded-lg md:w-10/12'>
             <TitleSecondary text="Adresse du bien à estimer" color="purple" />
-            <InputWithLogo image="/Vectorlocalization.png" marginY="my-4"/>
+            <InputWithLogo image="/Vectorlocalization.png" marginY="my-4" placeholder="Saisir une adresse" />
             <ButtonPrimary text="Estimer" version="primary" />
         </div>
     );
