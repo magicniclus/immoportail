3f9d511481e2de1fd09c172c24154aa6ed9e02a2
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 16:29:33 +0100

    Create QualityBanner.js

---
+++ components/Organisms/QualityBanner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SetOfQuality from '../Molecules/SetOfQuality';
+
+const QualityBanner = () => {
+    return (
+        <div>
+            <SetOfQuality image="Vectorcheck"/>
+        </div>
+    );
+};
+
+export default QualityBanner;
