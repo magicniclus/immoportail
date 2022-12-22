c010ed1ffe4db12a8b7bdc64b087bfcddcb9d184
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 17:35:48 +0100

    Update Icon.js

--- components/Atoms/logos/Icon.js
+++ components/Atoms/logos/Icon.js
@@ -6,7 +6,7 @@ const Icon = (props) => {
 
     return (
         <div className='w-16 md:w-24 h-16 md:h-24'>
-            <Image priority src={"/vector/"+image+".png"} height={100} width={100} alt="Icon" />
+            <Image priority src={"/vector/"+image+".png"} height={80} width={80} alt="Icon" />
         </div>
     );
 };
