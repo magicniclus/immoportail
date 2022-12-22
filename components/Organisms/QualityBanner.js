f067a271c8374141f186e030ed3da3a529dff910
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 17:35:54 +0100

    Update QualityBanner.js

--- components/Organisms/QualityBanner.js
+++ components/Organisms/QualityBanner.js
@@ -3,8 +3,12 @@ import SetOfQuality from '../Molecules/SetOfQuality';
 
 const QualityBanner = () => {
     return (
-        <div>
-            <SetOfQuality image="Vectorcheck"/>
+        <div className='w-full'>
+            <div className="flex-col lg:flex-row flex justify-between py-10 md:py-16 items-center px-5 md:px-40">
+                <SetOfQuality image="Vectorcheck" title="Estimation" titleWithColor="en ligne" text="Estimez votre bien en quelques clic grâce à notre outil d’estimation 100% gratuit, fiable, basé sur les données officiel mis à jour en temps réel." />
+                <SetOfQuality image="Vectorcheck" title="Estimation" titleWithColor="en ligne" text="Estimez votre bien en quelques clic grâce à notre outil d’estimation 100% gratuit, fiable, basé sur les données officiel mis à jour en temps réel." />
+                <SetOfQuality image="Vectorcheck" title="Estimation" titleWithColor="en ligne" text="Estimez votre bien en quelques clic grâce à notre outil d’estimation 100% gratuit, fiable, basé sur les données officiel mis à jour en temps réel." />
+            </div>
         </div>
     );
 };
