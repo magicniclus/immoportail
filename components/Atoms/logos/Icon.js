5b8f3ea81e7d55f6b60850d25b4407897d71bad5
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 16:29:27 +0100

    Create Icon.js

---
+++ components/Atoms/logos/Icon.js
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Icon = (props) => {
+    const image = props.image
+
+    return (
+        <div className='w-16 md:w-24 h-16 md:h-24'>
+            <Image priority src={"/vector/"+image+".png"} height={100} width={100} alt="Icon" />
+        </div>
+    );
+};
+
+export default Icon;
