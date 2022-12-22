20ea5517fde07783d164ffe678ff23f7a97ca3c0
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 14:20:01 +0100

    Update SetOfAdvice.js

--- components/Molecules/SetOfAdvice.js
+++ components/Molecules/SetOfAdvice.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Text from '../Atoms/texts/Text'
+import TitleWithLogo from '../Atoms/titles/TitleWithLogo';
 
 const SetOfAdvice = (props) => {
     const title = props.title
@@ -7,8 +8,9 @@ const SetOfAdvice = (props) => {
     const color = props.color
 
     return (
-        <div className=''>
-            <Text textSize="light" color="white">
+        <div className='lg:w-4/12 min-h-full rounded-lg bg-purple py-5 lg:py-0 px-11 flex justify-around flex-col my-4 lg:my-0'>
+            <TitleWithLogo text={title} image="Vectorhand" color="white" marginX="mb-8" />
+            <Text textSize="bigLight" color="white">
                 {text}
             </Text>
         </div>
