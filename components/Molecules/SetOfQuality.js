c271b345b2b138da34ff658b02cc1f4d49172cab
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 17:35:53 +0100

    Update SetOfQuality.js

--- components/Molecules/SetOfQuality.js
+++ components/Molecules/SetOfQuality.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Icon from '../Atoms/logos/Icon';
+import Text from '../Atoms/texts/Text';
+import TitleTertiary from '../Atoms/titles/TitleTertiary';
 
 const SetOfQuality = (props) => {
     const image = props.image
+    const title = props.title
+    const titleWithColor = props.titleWithColor
+    const text = props.text
     return (
-        <div>
+        <div className='flex flex-col items-center max-w-xs my-14 lg:my-0'>
             <Icon image={image} />
+            <TitleTertiary text={title} textWithColor={titleWithColor} color="purple" marginY="my-5" />
+            <Text color="purple" textSize="light" center="text-center">
+                {text}
+            </Text>
         </div>
     );
 };
