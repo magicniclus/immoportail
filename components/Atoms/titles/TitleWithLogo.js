b588b7a3c4ba59f53a7a52b7c9f2c048a376a6fc
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 14:19:59 +0100

    Update TitleWithLogo.js

--- components/Atoms/titles/TitleWithLogo.js
+++ components/Atoms/titles/TitleWithLogo.js
@@ -1,23 +1,30 @@
+import Image from 'next/image';
 import React from 'react';
 
 const TitleWithLogo = (props) => {
     const text = props.text;
     const textWithColor = props.textWithColor
     const color = props.color
+    const image = props.image
+    const marginX = props.marginX
+    const marginY = props.marginY
 
     const classDesign = {
-        white:"text-white text-2xl md:text-4xl font-bold md:w-10/12",
-        purple:"text-purple text-2xl md:text-4xl font-bold md:w-10/12"
+        white:"text-white text-2xl md:text-2xl font-bold md:w-10/12 lg:mb-0 " + marginX + " " + marginY,
+        purple:"text-purple text-2xl md:text-2xl font-bold md:w-10/12 lg:mb-0 " + marginX + " " + marginY
     }
 
     return (
-        <>
+        <div className='flex items-center'>
+            <div className={"h-4 w-6 lg:mb-0 " + marginX + " " + marginY}>
+                <Image src={"/vector/"+image+".png"} height={80} width={80} alt="icon"/>
+            </div>
             {
                 textWithColor !== undefined && textWithColor !== null ? 
-                <h1 className={color !== undefined ? classDesign[color] : classDesign.white}>{text}&nbsp;<span className='text-blue'>{textWithColor}</span></h1>:
-                <h1 className={color !== undefined ? classDesign[color] : classDesign.white}>{text}</h1>
+                <h1 className={color !== undefined ? classDesign[color] : classDesign.white}>&nbsp;{text}&nbsp;<span className='text-blue'>{textWithColor}</span></h1>:
+                <h1 className={color !== undefined ? classDesign[color] : classDesign.white}>&nbsp;{text}</h1>
             }
-        </>
+        </div>
     );
 };
 
