84fb6809f4e63ab0f2a20c4c41805ebbd788f705
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 16:29:31 +0100

    Create SetOfQuality.js

---
+++ components/Molecules/SetOfQuality.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Icon from '../Atoms/logos/Icon';
+
+const SetOfQuality = () => {
+    return (
+        <div>
+            <Icon image="Vectorcheck" />
+        </div>
+    );
+};
+
+export default SetOfQuality;
