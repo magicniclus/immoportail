2127ec64eae52bcd96a89cc8b8ef48d866265c6c
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 14:23:36 +0100

    Update InputWithLogo.js

--- components/Atoms/inputs/InputWithLogo.js
+++ components/Atoms/inputs/InputWithLogo.js
@@ -9,7 +9,7 @@ const InputWithLogo = (props) => {
     return (
         <div className={'relative sm:w-80 md:w-10/12 h-12 ' + marginY}>
             <input className='border rounded-lg border-purple placeholder-gray-200::placeholder px-3 w-full h-12' type="text" placeholder={placeholder}  />
-            <div className='absolute right-0 top-1/2 transform -translate-y-1/2 h-12 w-14 bg-purple flex content-center items-center rounded-r-lg'>
+            <div className='absolute right-0 top-1/2 transform -translate-y-1/2 h-12 w-14 bg-purple hidden sm:flex content-center items-center rounded-r-lg'>
                 <div className='mx-auto'>
                     <Image src={image} height={15} width={15} property objectFit="contain" objectPosition='right' alt="icon" />
                 </div>
