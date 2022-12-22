1dedf07cfa016e12690717f976f70a60fe656b1f
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-22 11:04:24 +0100

    Update Point.js

--- components/Atoms/others/Point.js
+++ components/Atoms/others/Point.js
@@ -2,9 +2,7 @@ import React, { useEffect } from 'react';
 
 const Point = (props) => {
     const active = props.active
-    useEffect(()=>{
-        console.log(active);
-    }, [])
+    
     return (
         <div className={active === true ? 'cursor-pointer h-4 w-4 bg-white opacity-100 rounded-full':'cursor-pointer h-4 w-4 bg-white opacity-40 rounded-full'}></div>
     );
