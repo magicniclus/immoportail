5e3f2402789dd2efb5d0241ef630e5a36a07995f
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-23 13:22:33 +0100

    Create Mozaic.js

---
+++ components/Molecules/Mozaic.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mozaic = () => {
+    return (
+        <div style={{background: "radial-gradient(circle, rgba(54,38,118,0.31278448879551823) 0%, rgba(255,255,255,0) 39%)"}} className='min-h-full min-w-full'>
+            
+        </div>
+    );
+};
+
+export default Mozaic;
