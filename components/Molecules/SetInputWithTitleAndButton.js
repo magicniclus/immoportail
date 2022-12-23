0d9801686decb4f6537ffd701f3cb89952bc4487
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-23 14:40:46 +0100

    Update SetInputWithTitleAndButton.js

--- components/Molecules/SetInputWithTitleAndButton.js
+++ components/Molecules/SetInputWithTitleAndButton.js
@@ -3,12 +3,17 @@ import TitleSecondary from "../Atoms/titles/TitleSecondary"
 import InputWithLogo from '../Atoms/inputs/InputWithLogo';
 import ButtonPrimary from '../Atoms/buttons/ButtonPrimary';
 
-const SetInputWithTitleAndButton = () => {
+const SetInputWithTitleAndButton = (props) => {
+    const bgColor = props.bgColor
+    const titleColor = props.titleColor
+    const buttonVersion = props.buttonVersion
+    const colorInput = props.colorInput
+
     return (
-        <div className='bg-white py-5 px-7 my-4 rounded-lg md:w-10/12'>
-            <TitleSecondary text="Adresse du bien à estimer" color="purple" />
-            <InputWithLogo image="/Vectorlocalization.png" marginY="my-4" placeholder="Saisir une adresse" />
-            <ButtonPrimary text="Estimer" version="primary" />
+        <div className={bgColor !== undefined ?'bg-'+ bgColor + ' py-5 px-7 my-4 rounded-lg md:w-10/12' : 'bg-white py-5 px-7 my-4 rounded-lg md:w-10/12'}>
+            <TitleSecondary text="Adresse du bien à estimer" color={titleColor !== undefined ? titleColor :"purple"} />
+            <InputWithLogo image={titleColor === "purple" ? "/Vectorlocalization.png" : "/vector/VectorlocalizationPurple.png"} marginY="my-4" placeholder="Saisir une adresse" color={colorInput !== undefined ? colorInput : "purple"} />
+            <ButtonPrimary text="Estimer" version={buttonVersion !== undefined ? buttonVersion :"primary"} />
         </div>
     );
 };
