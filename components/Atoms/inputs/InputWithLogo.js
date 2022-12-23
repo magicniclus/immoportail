bb31b92f7de0b6e8b229e4648dab0405d951f3f2
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-23 14:40:44 +0100

    Update InputWithLogo.js

--- components/Atoms/inputs/InputWithLogo.js
+++ components/Atoms/inputs/InputWithLogo.js
@@ -5,11 +5,12 @@ const InputWithLogo = (props) => {
     const placeholder = props.placeholder
     const image = props.image
     const marginY = props.marginY
+    const color = props.color
 
     return (
         <div className={'relative sm:w-80 md:w-10/12 h-12 ' + marginY}>
-            <input className='border rounded-lg border-purple placeholder-gray-200::placeholder px-3 w-full h-12' type="text" placeholder={placeholder}  />
-            <div className='absolute right-0 top-1/2 transform -translate-y-1/2 h-12 w-14 bg-purple hidden sm:flex content-center items-center rounded-r-lg'>
+            <input className={color !== undefined ? 'border rounded-lg placeholder-gray-200::placeholder px-3 w-full h-12 border-'+color :'border rounded-lg placeholder-gray-200::placeholder px-3 w-full h-12 border-purple'} type="text" placeholder={placeholder}  />
+            <div className={color !== undefined ? 'absolute right-0 top-1/2 transform -translate-y-1/2 h-12 w-14 hidden sm:flex content-center items-center rounded-r-lg bg-'+color :'absolute right-0 top-1/2 transform -translate-y-1/2 h-12 w-14 hidden sm:flex content-center items-center rounded-r-lg bg-purple'}>
                 <div className='mx-auto'>
                     <Image src={image} height={15} width={15} property objectFit="contain" objectPosition='right' alt="icon" />
                 </div>
