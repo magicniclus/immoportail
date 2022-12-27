2c2e5b82355b45d9bf187c94f470c025a87487a2
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-27 10:43:54 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,23 @@
-import Head from 'next/head'
 import { Inter } from '@next/font/google'
-import Header from '../components/Organisms/Header'
 import EstimationBanner from '../components/Organisms/EstimationBanner'
 import BreadcrumbContainer from '../components/Organisms/BreadcrumbContainer'
 import EvaluationBanner from '../components/Organisms/EvaluationBanner'
 import QualityBanner from '../components/Organisms/QualityBanner'
 import ImmoStepContainer from '../components/Organisms/ImmoStepContainer'
 import SetInputWithTitleButtonBanner from '../components/Organisms/SetInputWithTitleButtonBanner'
-import Footer from '../components/Organisms/Footer'
+import Layout from '../components/Layout/Layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>ImmoPortail | Accueil</title>
-        <meta property="og:title" content="Estimez votre bien immobilier gratuitement en ligne en 2 minutes" key="title" />
-      </Head>
-      <header>
-        <Header />
-      </header>
-      <main>
+    <Layout title="ImmoPortail | Accueil" description="Estimez votre bien immobilier gratuitement en ligne en 2 minutes">
         <EstimationBanner />
         <BreadcrumbContainer list={["Accueil", "Estimation"]} />
         <EvaluationBanner />
         <QualityBanner />
         <ImmoStepContainer />
-        <SetInputWithTitleButtonBanner /> 
-      </main>
-      <Footer />
-    </>
+         <SetInputWithTitleButtonBanner /> 
+    </Layout>
   )
 }
