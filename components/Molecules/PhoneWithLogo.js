bec52e6a610b3d15355eda1b9ee7c51c7b836d9a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-28 09:36:09 +0100

    Update PhoneWithLogo.js

--- components/Molecules/PhoneWithLogo.js
+++ components/Molecules/PhoneWithLogo.js
@@ -4,10 +4,11 @@ import TitleSecondary from '../Atoms/titles/TitleSecondary';
 
 const PhoneWithLogo = (props) => {
     const phoneNumber = props.phoneNumber
+    const text = props.text
     return (
         <a href={'tel:'+phoneNumber} className="flex items-center cursor-pointer">
             <Icon image="Vectorphone" size="extraLight"/>
-            <TitleSecondary text={phoneNumber}  color="purple" marginY="mx-2" />
+            <TitleSecondary text={text === undefined ? phoneNumber : text}  color="purple" marginY="mx-2" />
         </a> 
     );
 };
