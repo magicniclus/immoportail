5d5f8cd29b3c7e69adac3e298e6e5a39ee1b3b08
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-28 09:14:22 +0100

    Update ContactModal.js

--- components/Organisms/ContactModal.js
+++ components/Organisms/ContactModal.js
@@ -3,43 +3,36 @@ import Text from '../Atoms/texts/Text';
 import PhoneWithLogo from '../Molecules/PhoneWithLogo';
 import Icon from '../Atoms/logos/Icon';
 import Image from 'next/image';
+import TitleSecondary from '../Atoms/titles/TitleSecondary';
 
 
 const ContactModal = () => {
 
     const [toggleModal, setToggleModal]=useState(false)
 
-    const closeModal = ()=>{
+    const openModal = ()=>{
         return (
             <>
                 <Text textSize="normal" color="purple">
-                    Contactez directement <b>Nicolas</b> votre conseiller local:
+                    Pour un rendez-vous, une question sur votre projet ou estimation, contactez directement <b>Nicolas</b> votre conseiller local:
                 </Text>
                 <div className="flex mt-3 items-center">
                     <PhoneWithLogo phoneNumber="+33631420045" />
                 </div> 
-                <div onClick={e=>setToggleModal(true)} className="absolute w-10 h-10 right-1 bottom-0">
-                    <Icon image="vectorchevronBottom" size="light"/>
+                <div onClick={e=>setToggleModal(false)} className="absolute w-10 h-10 right-1 bottom-0">
+                    <Icon image="Vectorclose" size="extraLight" />
                 </div>
             </>
         )
     }
-
-    const openModal = ()=>{
+    
+    const closeModal = ()=>{
         return(
             <>
-                <Text textSize="normal" color="purple">
-                    Contactez directement <b>Nicolas</b> votre conseiller local:
-                </Text>
-                <div className="flex mt-5 items-center">
-                    <PhoneWithLogo phoneNumber="+33631420045" />
-                </div> 
-                <div onClick={e=>setToggleModal(false)} className="absolute w-10 h-10 right-1 bottom-0">
-                    <Icon image="vectorchevronTop" size="light"/>
+                <PhoneWithLogo phoneNumber="Contact" />
+                <div onClick={e=>setToggleModal(true)} className="absolute w-10 h-10 right-1 bottom-0">
+                    <Icon image="vectorchevronBottom" size="light"/>
                 </div>
-                <Text textSize="normal" color="purple" marginY="mt-3">
-                    Si vous avez une question concernant <b>votre projet</b>, le resultat de <b>votre estimation</b> ou si vous souhaitez un rendez-vous dans les 48h, contactez directement <b>Nicolas</b> qui se fera un plaisir de vous conseiller.
-                </Text>
             </>
         )
     }
@@ -49,7 +42,10 @@ const ContactModal = () => {
             <div className="absolute h-14 bg-white w-14 -top-6 -left-6 rounded-full overflow-hidden">
                 <Image priority src="/femme.jpg" layout='fill' objectFit='cover' alt='conseiller'/>
             </div>
-            <p className='text-orange absolute -top-4 left-10 text-xl font-bold bg-white py-1 px-3 rounded-lg'>Nouveau</p>
+            {
+                !toggleModal ? <p className='text-orange absolute -top-4 left-10 text-xl font-bold bg-white py-1 px-3 rounded-lg'>Nouveau</p> : null
+            }
+            
             {toggleModal ? openModal() : closeModal()}
         </div>
     );
