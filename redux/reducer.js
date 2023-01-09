cf6c3cf5ac804290788a4da628fd269f759d657a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-09 20:09:06 +0100

    Update reducer.js

--- redux/reducer.js
+++ redux/reducer.js
@@ -1,9 +1,21 @@
 const initState = {
+    addressIsValid: false,
+    adress: ""
 }
 
 const reducer = ((state= initState, action)=>{
     switch(action.type) {
- 
+        case "updateAddressIsValid":
+            return{
+                ...state,
+                adressIsValid: action.payload
+            }
+
+        case "updateAdress":
+            return{
+                ...state,
+                adress: action.payload
+            }    
             
         default: return state    
     }
