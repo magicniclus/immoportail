b50c75e088c73d64b574c76fb07166156df8b42d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-09 17:47:37 +0100

    Update LightBox.js

--- components/Molecules/LightBox.js
+++ components/Molecules/LightBox.js
@@ -5,18 +5,23 @@ import Point from '../Atoms/others/Point';
 const LightBox = (props) => {
     const [whatPointIsActive, setWhatPointIsActive] = useState(0)
     const image = ["six", "two", "three", "four", "five"]
+    const [lightboxIsActive, setLiveboxIsActive]=useState(false)
 
     useEffect(()=>{
-        const interval = setInterval(()=>{
-            if(whatPointIsActive >= 0)setWhatPointIsActive(whatPointIsActive=>whatPointIsActive+1) 
-            if(whatPointIsActive >= 4) setWhatPointIsActive(whatPointIsActive=> 0)
-        }, 4000)
-        return ()=>clearInterval(interval)
+        if(lightboxIsActive){
+            const interval = setInterval(()=>{
+                if(whatPointIsActive >= 0)setWhatPointIsActive(whatPointIsActive=>whatPointIsActive+1) 
+                if(whatPointIsActive >= 4) setWhatPointIsActive(whatPointIsActive=> 0)
+            }, 4000)
+            return ()=>clearInterval(interval)
+        }else{
+            null
+        }
     }, [whatPointIsActive])
 
     return (
         <div className='flex content-center items-center relative w-full h-full'>
-            <div className="w-full h-full animate-[wiggle_1s_ease-in-out]">
+            <div className="relative w-full h-full animate-[wiggle_1s_ease-in-out]">
                 <Image priority src={"/lightbox/" + image[whatPointIsActive]+".jpg"} layout='fill' objectFit='cover' alt={image[whatPointIsActive]}/>
             </div>
             <div className="w-2/6 flex justify-around items-center absolute bottom-5 left-1/2 transform -translate-x-1/2 -translate-y-2/2">
