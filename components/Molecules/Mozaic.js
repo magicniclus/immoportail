58674e4bffe014883a0950ae3978cddbb228ee83
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-09 17:47:39 +0100

    Update Mozaic.js

--- components/Molecules/Mozaic.js
+++ components/Molecules/Mozaic.js
@@ -7,25 +7,25 @@ const Mozaic = () => {
             <div className="h-3/6 min-w-full flex justify-between">
                 <div className="h-full w-6/12 flex flex-col justify-between">
                     <div className="h-2/6 w-full relative rounded-2xl overflow-hidden">
-                        <Image priority src="/houses/one.jpg" layout='fill' objectFit='cover' />
+                        <Image priority src="/houses/one.jpg" layout='fill' objectFit='cover' alt="house" />
                     </div>
                     <div className="h-2/6 w-full relative rounded-2xl overflow-hidden">
-                        <Image priority src="/houses/two.jpg" layout='fill' objectFit='cover' />
+                        <Image priority src="/houses/two.jpg" layout='fill' objectFit='cover' alt="house" />
                     </div>
                 </div>
                 <div className="h-full w-5/12">
                     <div className="h-5/6 w-full relative rounded-2xl overflow-hidden">
-                        <Image priority src="/houses/three.jpg" layout='fill' objectFit='cover' />
+                        <Image priority src="/houses/three.jpg" layout='fill' objectFit='cover' alt="house" />
                     </div>
                 </div>
             </div>
             <div className="h-3/6 min-w-full mt-10 flex flex-col justify-between">
                 <div className="h-3/6 w-full relative rounded-2xl overflow-hidden">
-                    <Image priority src="/houses/four.jpg" layout='fill' objectFit='cover' />
+                    <Image priority src="/houses/four.jpg" layout='fill' objectFit='cover' alt="house" />
                 </div>
                 <div className="w-6/6 h-2/6 flex justify-end items-end">
                     <div className="w-3/6 h-full relative rounded-2xl overflow-hidden">
-                        <Image priority src="/houses/five.jpg" layout='fill' objectFit='cover' />
+                        <Image priority src="/houses/five.jpg" layout='fill' objectFit='cover' alt="house" />
                     </div>
                 </div>
             </div>
