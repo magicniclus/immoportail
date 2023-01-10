0334345d677f1e1d8f6554d398a966e07e36f8e2
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-10 10:12:25 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,23 @@ import Layout from '../components/Layout/Layout'
 import ContactModal from '../components/Organisms/ContactModal'
 import GoogleSearchInputWithLogo from '../components/Atoms/inputs/googleSearchInputWithLogo'
 import AdressForm from '../components/Molecules/form/AdressForm'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [disabled, setDisabled]=useState(true)
+
+  const state = useSelector(state=>state)
+
+  useEffect(()=>{
+    state.address !== ""? setDisabled(false):setDisabled(true)
+  }, [state.address])
+
   return (
     <Layout title="Estimez votre bien | ImmoPortail" description="Estimez votre bien immobilier gratuitement en ligne en 2 minutes">
-        <EstimationBanner />
+        <EstimationBanner disabled={disabled} />
         <BreadcrumbContainer list={["Accueil", "Estimation"]} />
         <EvaluationBanner />
         <QualityBanner />
