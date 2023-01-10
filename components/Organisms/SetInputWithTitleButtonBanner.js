363f3e94508a670a962405fbadacdb3f165afcac
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-10 10:32:30 +0100

    Update SetInputWithTitleButtonBanner.js

--- components/Organisms/SetInputWithTitleButtonBanner.js
+++ components/Organisms/SetInputWithTitleButtonBanner.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SetInputWithTitleAndButton from '../Molecules/SetInputWithTitleAndButton'
 
-const SetInputWithTitleButtonBanner = () => {
+const SetInputWithTitleButtonBanner = (props) => {
+    const disabled = props.disabled
+
     return (
         <div className='px-5 md:px-40 py-10 md:py-16'>
             <div className="lg:w-8/12">
-                <SetInputWithTitleAndButton textWithColor="gratuitement en ligne" bgColor="purple" titleColor="white" buttonVersion="secondary" colorInput="white" />
+                <SetInputWithTitleAndButton disabled={disabled} textWithColor="gratuitement en ligne" bgColor="purple" titleColor="white" buttonVersion="secondary" colorInput="white" />
             </div>
         </div>
     );
