ca4c958cf3470919379798532c580624e1c1c2ed
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-10 10:12:20 +0100

    Update SetInputWithTitleAndButton.js

--- components/Molecules/SetInputWithTitleAndButton.js
+++ components/Molecules/SetInputWithTitleAndButton.js
@@ -2,6 +2,8 @@ import React from 'react';
 import TitleSecondary from "../Atoms/titles/TitleSecondary"
 import InputWithLogo from '../Atoms/inputs/InputWithLogo';
 import ButtonPrimary from '../Atoms/buttons/ButtonPrimary';
+import { Dispatch } from 'redux';
+import { useDispatch } from 'react-redux';
 
 const SetInputWithTitleAndButton = (props) => {
     const bgColor = props.bgColor
@@ -9,12 +11,15 @@ const SetInputWithTitleAndButton = (props) => {
     const buttonVersion = props.buttonVersion
     const colorInput = props.colorInput
     const textWithColor = props.textWithColor
+    const disabled = props.disabled
+
+    const dispatch = useDispatch()
 
     return (
         <div className={bgColor !== undefined ?'bg-'+ bgColor + ' py-5 px-7 my-4 rounded-lg md:w-10/12' : 'bg-white py-5 px-7 my-4 rounded-lg md:w-10/12'}>
             <TitleSecondary text="Adresse du bien à estimer" textWithColor={textWithColor} color={titleColor !== undefined ? titleColor :"purple"} />
             <InputWithLogo componentIsForAdress={true} image={titleColor === "purple" ? "/Vectorlocalization.png" : "/vector/VectorlocalizationPurple.png"} marginY="my-4" placeholder="Saisir une adresse" color={colorInput !== undefined ? colorInput : "purple"} />
-            <ButtonPrimary text="Estimer" version={buttonVersion !== undefined ? buttonVersion :"primary"} />
+            <ButtonPrimary disabled={disabled} text="Estimer" version={buttonVersion !== undefined ? buttonVersion :"primary"} />
         </div>
     );
 };
