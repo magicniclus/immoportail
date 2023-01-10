53151b6edcafc89b9af7d44f01d781b196cd508f
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-10 13:24:40 +0100

    Update AdressForm.js

--- components/Molecules/form/AdressForm.js
+++ components/Molecules/form/AdressForm.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import axios, { CancelToken } from 'axios'
 import { useDispatch } from 'react-redux'
 import { updateAddress, updateAddressCoordinate } from '../../../redux/action'
 
 const AdressForm = (props) => {
+    
     const placeholder = props.placeholder
     const color = props.color
+    const [source, setSource] = useState(axios.CancelToken.source())
 
     const API_KEY = 'AIzaSyBhFIY1nvseuxoi4xA0HPiM-PvwNQdx9kI'
     // Déclarez un état local pour stocker l'adresse saisie par l'utilisateur
@@ -19,10 +21,10 @@ const AdressForm = (props) => {
 
     // Utilisez useEffect pour récupérer les suggestions d'adresse à mesure que l'utilisateur saisit l'adresse
     useEffect(() => {
-
         setSuggestionsAreOpen(false)
         if ( address.trim() === '') {
             setSuggestions([])
+            setSource(axios.CancelToken.source())
             dispatch(updateAddress(""))
             return
         }
@@ -31,27 +33,35 @@ const AdressForm = (props) => {
             return
         }else{
             axios.get(
-                `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${address}&key=${API_KEY}&components=country:fr`
+                `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${address}&key=${API_KEY}&components=country:fr&types=address`, 
+                { cancelToken: source.token }
             )
             .then(res => {
                 setSuggestions(res.data.predictions)
             })
             .catch(error => {
-                console.error(error)
-                setSuggestions("Erreur, veuillez essayer à nouveau")
+                if (axios.isCancel(error)) {
+                console.log('Request canceled', error.message)
+                } else {
+                    console.error(error)
+                    setSuggestions("Erreur, veuillez essayer à nouveau")
+                }
             })
         }
     }, [address])
     
     const handleAddress = (description) => {
+        source.cancel(`Cancel request for ${address}`)
         setSuggestions([])
         setAddress(description)
+        setSource(axios.CancelToken.source())
         dispatch(updateAddress(description))
     }
     
     useEffect(()=>{
         if(suggestions.length > 0) setSuggestionsAreOpen(true)
         else setSuggestionsAreOpen(false)
+        
     }, [suggestions])
 
     return (
@@ -69,7 +79,7 @@ const AdressForm = (props) => {
                 suggestionsAreOpen ?
                 <ul className={'bg-white py-1'}>
                     {Array.isArray(suggestions) && suggestions.map(suggestion => (
-                        <li onClick={()=>handleAddress(suggestion.description)}className="text-purple cursor-pointer mx-3 my-3 hover:font-bold z-50" key={suggestion.id}>{suggestion.description}</li>
+                        <li onClick={()=>handleAddress(suggestion.description)} className="text-purple cursor-pointer mx-3 my-3 hover:font-bold z-50" key={suggestion.id}>{suggestion.description}</li>
                     ))}
                 </ul>:
                 null
