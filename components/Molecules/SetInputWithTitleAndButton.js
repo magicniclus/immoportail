809528c827f8cadf00153a30913c532c5fa57e03
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-10 10:54:08 +0100

    Update SetInputWithTitleAndButton.js

--- components/Molecules/SetInputWithTitleAndButton.js
+++ components/Molecules/SetInputWithTitleAndButton.js
@@ -11,11 +11,15 @@ const SetInputWithTitleAndButton = (props) => {
     const textWithColor = props.textWithColor
     const disabled = props.disabled
 
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+    }
+
     return (
-        <form className={bgColor !== undefined ?'bg-'+ bgColor + ' py-5 px-7 my-4 rounded-lg md:w-10/12' : 'bg-white py-5 px-7 my-4 rounded-lg md:w-10/12'}>
+        <form onSubmit={handleSubmit} className={bgColor !== undefined ?'bg-'+ bgColor + ' py-5 px-7 my-4 rounded-lg md:w-10/12' : 'bg-white py-5 px-7 my-4 rounded-lg md:w-10/12'}>
             <TitleSecondary text="Adresse du bien à estimer" textWithColor={textWithColor} color={titleColor !== undefined ? titleColor :"purple"} />
             <InputWithLogo componentIsForAdress={true} image={titleColor === "purple" ? "/Vectorlocalization.png" : "/vector/VectorlocalizationPurple.png"} marginY="my-4" placeholder="Saisir une adresse" color={colorInput !== undefined ? colorInput : "purple"} />
-            <ButtonPrimary disabled={disabled} text="Estimer" version={buttonVersion !== undefined ? buttonVersion :"primary"} />
+            <ButtonPrimary type="submit" disabled={disabled} text="Estimer" version={buttonVersion !== undefined ? buttonVersion :"primary"} />
         </form>
     );
 };
