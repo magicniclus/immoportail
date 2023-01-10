22765259198fa0ca28acb38cd18d2399ef08a847
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-10 10:12:14 +0100

    Update ButtonPrimary.js

--- components/Atoms/buttons/ButtonPrimary.js
+++ components/Atoms/buttons/ButtonPrimary.js
@@ -4,14 +4,15 @@ const ButtonPrimary = (props) => {
     const text = props.text
     const version = props.version
     const callback = props.callback
+    const disabled = props.disabled || false
 
     const classDesign = {
-        primary: "text-base px-10 py-2 bg-purple rounded-3xl text-white hover:shadow-md hover:shadow-slate-200 transition-shadow",
-        secondary: "text-base px-10 py-2 bg-white rounded-3xl text-purple hover:shadow-md hover:shadow-slate-200 transition-shadow"
+        primary: disabled ? "text-base px-10 py-2 bg-purpleLight rounded-3xl text-white hover:shadow-md hover:shadow-slate-200 transition-shadow": "text-base px-10 py-2 bg-purple rounded-3xl text-white hover:shadow-md hover:shadow-slate-200 transition-shadow",
+        secondary: disabled ? "text-base px-10 py-2 bg-white rounded-3xl text-purple hover:shadow-md hover:shadow-slate-200 transition-shadow":"text-base px-10 py-2 bg-white rounded-3xl text-purple hover:shadow-md hover:shadow-slate-200 transition-shadow"
     }
 
     return (
-        <button className={version !== undefined && version !== null ? classDesign[version] : classDesign.primary}>
+        <button disabled={disabled} className={version !== undefined && version !== null ? classDesign[version] : classDesign.primary}>
             {text}
         </button>
     );
