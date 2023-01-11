e78ede4f4eed29d816d5c74166ddc2738370101f
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-11 16:31:27 +0100

    Update Footer.js

--- components/Organisms/Footer.js
+++ components/Organisms/Footer.js
@@ -7,30 +7,32 @@ import ButtonPrimary from '../Atoms/buttons/ButtonPrimary';
 
 const Footer = () => {
     return (
-        <footer className="w-full py-20 px-5 lg:px-40 lg:flex lg:justify-between bg-gray-300 lg:flex-row flex-col">
-            <div className="lg:w-4/12 w-8/12">
-                <ul>
-                    <li className='mb-3 text-purple text-base md:text-xl'>
-                        <Link href="/">Mon espace</Link>
-                    </li>
-                    <li className='mb-3 text-purple text-base md:text-xl'>
-                        <Link href="/">Espace partenaire</Link>
-                    </li>
-                    <li className='mb-3 text-purple text-base md:text-xl'>
-                        <Link href="/">Nous contacter</Link>
-                    </li>
-                    <li className='mb-3 text-purple text-base md:text-xl'>
-                        <Link href="/">Politique de confidentialité</Link>
-                    </li>
-                </ul>
-                <p className='text-purple text-base md:text-xl'>© immoportail.fr 2022</p>
+        <footer className="w-full py-20 px-5 lg:px-40 lg:flex lg:justify-between bg-gray-300 lg:flex-row flex-col drop-shadow-md ">
+            <div className=" max-w-screen-2xl flex justify-between w-full lg:flex lg:justify-between lg:flex-row flex-col mx-auto">
+                <div className="lg:w-4/12 w-8/12">
+                    <ul>
+                        <li className='mb-3 text-purple text-base md:text-xl'>
+                            <Link href="/">Mon espace</Link>
+                        </li>
+                        <li className='mb-3 text-purple text-base md:text-xl'>
+                            <Link href="/">Espace partenaire</Link>
+                        </li>
+                        <li className='mb-3 text-purple text-base md:text-xl'>
+                            <Link href="/">Nous contacter</Link>
+                        </li>
+                        <li className='mb-3 text-purple text-base md:text-xl'>
+                            <Link href="/">Politique de confidentialité</Link>
+                        </li>
+                    </ul>
+                    <p className='text-purple text-base md:text-xl'>© immoportail.fr 2022</p>
+                </div>
+                <form className="flex flex-col justify-between items-start lg:items-end lg:w-4/12 w-8/12 lg:h-auto h-64 my-7 lg:m-0">
+                    <Logo />
+                    <TitleSecondary text="S’inscrire à notre news letters" color="purple" />
+                    <InputWithLogo placeholder="Saisir un email" image="/vector/Vectorletter.png" color="white" />
+                    <ButtonPrimary text="S'inscrire" version="primary" />
+                </form>
             </div>
-            <form className="flex flex-col justify-between items-start lg:items-end lg:w-4/12 w-8/12 lg:h-auto h-64 my-7 lg:m-0">
-                <Logo />
-                <TitleSecondary text="S’inscrire à notre news letters" color="purple" />
-                <InputWithLogo placeholder="Saisir un email" image="/vector/Vectorletter.png" color="white" />
-                <ButtonPrimary text="S'inscrire" version="primary" />
-            </form>
         </footer>
     );
 };
