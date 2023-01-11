a50029e6645df1d25ee8b3f322bc8af58114b732
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-11 13:58:10 +0100

    Create HeaderEstimation.js

---
+++ components/Organisms/headers/HeaderEstimation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../../Atoms/logos/Logo';
+import TitleSecondary from '../../Atoms/titles/TitleSecondary';
+
+const HeaderEstimation = () => {
+    return (
+        <div className='w-screen z-20 bg-white h-28 drop-shadow-md flex items-center place-content-between px-5 lg:px-40'>
+            <div className="flex flex-col items-baseline lg:flex-row">
+                {/* Left container */}
+                <Logo />
+                {/* Right container */}
+                <div className="ml-5">
+                    <TitleSecondary text="Votre estimation en ligne en " textWithColor="2 minutes" color="purple"/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HeaderEstimation;
