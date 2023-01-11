e26acfa16b5c39c5a938de2a344c9dd1b6cb8439
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-11 15:46:43 +0100

    Update index.js

--- pages/estimation-immobiliere/index.js
+++ pages/estimation-immobiliere/index.js
@@ -4,7 +4,11 @@ import LayoutEstimation from '../../components/Layout/LayoutEstimation';
 const EstimationImmobiliere = () => {
     return (
         <LayoutEstimation>
-            
+            {/* Left Container */}
+            <div className="min-h-full w-5/12 bg-grayLight">
+                
+            </div>
+            {/* Right Container */}
         </LayoutEstimation>
     );
 };
