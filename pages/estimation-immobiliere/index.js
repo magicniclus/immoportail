2bdbae3d8a8fb384f0c85b59fa79687db8fdce83
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-11 16:31:32 +0100

    Update index.js

--- pages/estimation-immobiliere/index.js
+++ pages/estimation-immobiliere/index.js
@@ -5,8 +5,9 @@ const EstimationImmobiliere = () => {
     return (
         <LayoutEstimation>
             {/* Left Container */}
-            <div className="min-h-full w-5/12 bg-grayLight">
-                
+            <div className="min-h-full w-5/12 bg-grayLight flex justify-end">
+                 <div className="w-full 2xl:w-10/12 bg-purple"></div>
+                 
             </div>
             {/* Right Container */}
         </LayoutEstimation>
