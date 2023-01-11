a50d850745a3fc91b215b0573c2f7e09b7c03c83
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-11 15:46:40 +0100

    Update HeaderEstimation.js

--- components/Organisms/headers/HeaderEstimation.js
+++ components/Organisms/headers/HeaderEstimation.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import Logo from '../../Atoms/logos/Logo';
 import TitleSecondary from '../../Atoms/titles/TitleSecondary';
@@ -7,7 +8,9 @@ const HeaderEstimation = () => {
         <div className='w-screen z-20 bg-white h-28 drop-shadow-md flex items-center place-content-between px-5 lg:px-40'>
             <div className="flex flex-col items-baseline lg:flex-row">
                 {/* Left container */}
-                <Logo />
+                <Link href="/">
+                    <Logo />
+                </Link>
                 {/* Right container */}
                 <div className="ml-5">
                     <TitleSecondary text="Votre estimation en ligne en " textWithColor="2 minutes" color="purple"/>
