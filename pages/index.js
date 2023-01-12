1d7a522f6a64cd98941572bae9c9acc75066a46e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-12 18:33:51 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,45 @@
-import { Inter } from '@next/font/google'
-import EstimationBanner from '../components/Organisms/EstimationBanner'
-import BreadcrumbContainer from '../components/Organisms/BreadcrumbContainer'
-import EvaluationBanner from '../components/Organisms/EvaluationBanner'
-import QualityBanner from '../components/Organisms/QualityBanner'
-import ImmoStepContainer from '../components/Organisms/ImmoStepContainer'
-import SetInputWithTitleButtonBanner from '../components/Organisms/SetInputWithTitleButtonBanner'
-import Layout from '../components/Layout/Layout'
-import ContactModal from '../components/Organisms/ContactModal'
-import GoogleSearchInputWithLogo from '../components/Atoms/inputs/googleSearchInputWithLogo'
-import AdressForm from '../components/Molecules/form/AdressForm'
-import { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { Inter } from "@next/font/google";
+import EstimationBanner from "../components/Organisms/EstimationBanner";
+import BreadcrumbContainer from "../components/Organisms/BreadcrumbContainer";
+import EvaluationBanner from "../components/Organisms/EvaluationBanner";
+import QualityBanner from "../components/Organisms/QualityBanner";
+import ImmoStepContainer from "../components/Organisms/ImmoStepContainer";
+import SetInputWithTitleButtonBanner from "../components/Organisms/SetInputWithTitleButtonBanner";
+import Layout from "../components/Layout/Layout";
+import ContactModal from "../components/Organisms/ContactModal";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateAddress, updateAddressCoordinate } from "../redux/action";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [disabled, setDisabled]=useState(true)
+  const [disabled, setDisabled] = useState(true);
 
-  const state = useSelector(state=>state)
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    state.address !== ""? setDisabled(false):setDisabled(true)
-  }, [state.address])
+  useEffect(() => {
+    state.address !== "" ? setDisabled(false) : setDisabled(true);
+  }, [state.address]);
+
+  useEffect(() => {
+    dispatch(updateAddress(""));
+    dispatch(updateAddressCoordinate(null));
+  }, []);
 
   return (
-    <Layout title="Estimez votre bien | ImmoPortail" description="Estimez votre bien immobilier gratuitement en ligne en 2 minutes">
-        <EstimationBanner disabled={disabled} />
-        <BreadcrumbContainer list={["Accueil", "Estimation"]} />
-        <EvaluationBanner />
-        <QualityBanner />
-        <ImmoStepContainer />
-        <SetInputWithTitleButtonBanner disabled={disabled}/> 
-        <ContactModal />
+    <Layout
+      title="Estimez votre bien | ImmoPortail"
+      description="Estimez votre bien immobilier gratuitement en ligne en 2 minutes"
+    >
+      <EstimationBanner disabled={disabled} />
+      <BreadcrumbContainer list={["Accueil", "Estimation"]} />
+      <EvaluationBanner />
+      <QualityBanner />
+      <ImmoStepContainer />
+      <SetInputWithTitleButtonBanner disabled={disabled} />
+      <ContactModal />
     </Layout>
-  )
+  );
 }
