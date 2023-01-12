8fc59ec8cbef8952fb68fb8de99ab6032b3f5977
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-12 16:48:01 +0100

    Create BottomStepContainer.js

---
+++ components/Organisms/BottomStepContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BottomStepContainer = () => {
+    return (
+        <div className='h-24 w-full bg-grayLight block md:hidden'>
+            
+        </div>
+    );
+};
+
+export default BottomStepContainer;
