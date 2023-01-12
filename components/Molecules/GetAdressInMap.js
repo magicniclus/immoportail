d022920166725c8be81afd221d673652001501d8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-12 16:47:57 +0100

    Create GetAdressInMap.js

---
+++ components/Molecules/GetAdressInMap.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GetAdressInMap = () => {
+    return (
+        <div className='h-56 w-10/12 lg:ml-14 lg:w-7/12 bg-blue'>
+            
+        </div>
+    );
+};
+
+export default GetAdressInMap;
