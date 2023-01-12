6c40d50cb71c7f4c3b07dffafee3f5d7b4c03151
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-12 16:47:53 +0100

    Create TextWithColorChange.js

---
+++ components/Atoms/texts/TextWithColorChange.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const TextWithColorChange = (props) => {
+    const text = props.text
+    const isSelected = props.isSelected
+    const valueIdx = props.valueIdx
+    const updateStyle = props.updateStyle
+
+    console.log(valueIdx);
+
+    return (
+        <p className={isSelected ? `text-sm text-newGray font-semibold ${updateStyle}`:`text-xs text-purple font-semibold ${updateStyle}`}>
+            {text} 
+        </p>
+    );
+};
+
+export default TextWithColorChange;
+
+// 2xl: v md:min-h-[calc((100vh-7rem))]
