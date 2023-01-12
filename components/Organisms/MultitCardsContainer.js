2e01e73ed97dfc3d8a3efbc2323bb6c1bff3e41b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-12 16:48:07 +0100

    Create MultitCardsContainer.js

---
+++ components/Organisms/MultitCardsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MultitCardsContainer = () => {
+    return (
+        <div className="md:w-7/12 w-full ">
+            
+        </div>
+    );
+};
+
+export default MultitCardsContainer;
