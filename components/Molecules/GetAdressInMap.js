e4214baef98840fc315f4fdf0366602b97bfdce4
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-13 14:40:31 +0100

    Update GetAdressInMap.js

--- components/Molecules/GetAdressInMap.js
+++ components/Molecules/GetAdressInMap.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from "react";
 import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
 import { useSelector } from "react-redux";
 
-const GetAdressInMap = () => {
+const GetAdressInMap = (props) => {
+  const isOk = props.isOk;
+
+  const [scriptLoaded, setScriptLoaded] = useState(false);
   const state = useSelector((state) => state);
 
   const containerStyle = {
@@ -22,33 +25,50 @@ const GetAdressInMap = () => {
     zoomControl: false,
   };
 
-  const [scriptLoaded, setScriptLoaded] = useState(false);
-
   useEffect(() => {
-    if (!scriptLoaded && state.addressCoordinate !== null) {
+    if (state.address !== "") {
+      console.log(state.addressCoordinate);
       setScriptLoaded(true);
     }
-  }, [scriptLoaded, state.addressCoordinate]);
+  }, [state.addressCoordinate]);
+
+  const getLoading = () => {
+    console.log("ok");
+    return (
+      <LoadScript googleMapsApiKey="AIzaSyBhFIY1nvseuxoi4xA0HPiM-PvwNQdx9kI">
+        <GoogleMap
+          id="map"
+          mapContainerStyle={containerStyle}
+          zoom={17}
+          center={center}
+          options={options}
+          style={{ zIndex: 0 }}
+        >
+          <Marker position={center} />
+        </GoogleMap>
+      </LoadScript>
+    );
+  };
+
+  const getComponent = () => {
+    console.log("ok deux");
+    return (
+      <GoogleMap
+        id="map"
+        mapContainerStyle={containerStyle}
+        zoom={17}
+        center={center}
+        options={options}
+        style={{ zIndex: 0 }}
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    );
+  };
 
   return (
     <div className="h-48 w-11/12 bg-white shadow-[inset_0px_0px_15px_30px_#FAFAFA]">
-      {scriptLoaded && (
-        <LoadScript
-          googleMapsApiKey="AIzaSyBhFIY1nvseuxoi4xA0HPiM-PvwNQdx9kI"
-          onLoad={() => setScriptLoaded(true)}
-        >
-          <GoogleMap
-            id="map"
-            mapContainerStyle={containerStyle}
-            zoom={17}
-            center={center}
-            options={options}
-            style={{ zIndex: 0 }}
-          >
-            <Marker position={center} />
-          </GoogleMap>
-        </LoadScript>
-      )}
+      {!scriptLoaded ? getLoading() : getComponent()}
     </div>
   );
 };
