2becf6061cd9ea81aeb8cea461f9fadf0c35363d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-13 13:21:29 +0100

    Update GetProjectProgress.js

--- components/Molecules/GetProjectProgress.js
+++ components/Molecules/GetProjectProgress.js
@@ -1,51 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import Icon from '../Atoms/logos/Icon';
-import TextWithColorChange from '../Atoms/texts/TextWithColorChange';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Icon from "../Atoms/logos/Icon";
+import TextWithColorChange from "../Atoms/texts/TextWithColorChange";
 
 const GetProjectProgress = () => {
-    const state = useSelector(state=>state)
-    const [trackStep, setTrackStep]=useState(state.stepOfProjectProgress)
+  const state = useSelector((state) => state);
+  const [trackStep, setTrackStep] = useState(state.stepOfProjectProgress);
 
-    const stepName = ["Adresse", "Type de bien", "Surface", "Nombre de pièce", "Année de construction", "Travaux", "Surface", "Espaces exterieurs", "Exposition/Vue", "Standing", "Mitoyenneté", "Profil"]
+  const stepName = [
+    "Adresse",
+    "Type de bien",
+    "Surface",
+    "Nombre de pièce",
+    "Année de construction",
+    "Travaux",
+    "Surface",
+    "Espaces exterieurs",
+    "Exposition/Vue",
+    "Standing",
+    "Mitoyenneté",
+    "Profil",
+  ];
 
-    useEffect(()=>{
+  useEffect(() => {
     if (trackStep !== state.stepOfProjectProgress) {
-        setTrackStep(state.stepOfProjectProgress);
+      setTrackStep(state.stepOfProjectProgress);
     }
-    },[state.stepOfProjectProgress])
+  }, [state.stepOfProjectProgress]);
 
-    const selectedBefore = "before:bg-purple"
-    const unselectedBefore = "before:bg-newGray"
+  const selectedBefore = "before:bg-purple";
+  const unselectedBefore = "before:bg-newGray";
 
-    const beforeGlobalStyle = "before:w-1 before:h-3 before:absolute before:bottom-4 before:left-1.5 z-40 "
+  const beforeGlobalStyle =
+    "before:w-1 before:h-3 before:absolute before:bottom-4 before:left-1.5 z-40 ";
 
-    const updateBefore = (idx, step)=>{
-        if(idx === 0){
-            return
-        }
-        if(idx < step ){
-            return beforeGlobalStyle + selectedBefore
-        }else{
-            return beforeGlobalStyle + unselectedBefore
-        }
+  const updateBefore = (idx, step) => {
+    if (idx === 0) {
+      return;
     }
+    if (idx < step) {
+      return beforeGlobalStyle + selectedBefore;
+    } else {
+      return beforeGlobalStyle + unselectedBefore;
+    }
+  };
 
-    return (
-        <div className='h-full min-h-max w-6/12 my-5 flex flex-col justify-between'>
-            {
-                stepName.map((item, idx)=>(
-                    <div className='flex'>  
-                        <div className={`h-full flex justify-center mb-3 items-center relative z-0 ${ updateBefore(idx, trackStep)}`}>
-                            <Icon image={trackStep > idx ? "checkRoundedPurple" : "roundedGray"} size="extraLight" /> 
-                        </div>
-                        <TextWithColorChange key={idx} text={item} isSelected={trackStep <= idx} valueIdx={idx} updateStyle="ml-3"/>
-                    </div>
-                    )
-                )
-            }
+  return (
+    <div className="h-full min-h-max w-6/12 my-5 flex flex-col justify-between">
+      {stepName.map((item, idx) => (
+        <div className="flex">
+          <div
+            className={`h-full flex justify-center mb-3 items-center relative z-0 ${updateBefore(
+              idx,
+              trackStep
+            )}`}
+          >
+            <Icon
+              image={trackStep > idx ? "checkRoundedPurple" : "roundedGray"}
+              size="extraLight"
+            />
+          </div>
+          <TextWithColorChange
+            key={idx}
+            text={item}
+            isSelected={trackStep <= idx}
+            valueIdx={idx}
+            updateStyle="ml-3"
+          />
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default GetProjectProgress;
