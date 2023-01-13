fcb3655185e24cd383e518bec7d4fb42dec8495c
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-13 13:21:36 +0100

    Update index.js

--- pages/estimation-immobiliere/index.js
+++ pages/estimation-immobiliere/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import LayoutEstimation from '../../components/Layout/LayoutEstimation';
-import BottomStepContainer from '../../components/Organisms/BottomStepContainer';
-import MultitCardsContainer from '../../components/Organisms/MultitCardsContainer';
-import ProjectStepContainer from '../../components/Organisms/ProjectStepContainer';
+import React from "react";
+import LayoutEstimation from "../../components/Layout/LayoutEstimation";
+import BottomStepContainer from "../../components/Organisms/BottomStepContainer";
+import MultitCardsContainer from "../../components/Organisms/MultitCardsContainer";
+import ProjectStepContainer from "../../components/Organisms/ProjectStepContainer";
 
 const EstimationImmobiliere = () => {
-    return (
-        <LayoutEstimation>
-            <div className="min-h-[calc((100vh-7rem))] w-full flex mx-auto flex-col justify-end md:flex-row md:max-w-screen-2xl md:px-5 md:py-8">
-                {/* Left Container */}
-                <ProjectStepContainer />
-                {/* Right Container */}
-                <MultitCardsContainer />
-                {/* Bottom Container */}
-                <BottomStepContainer />
-            </div>
-        </LayoutEstimation>
-    );
+  return (
+    <LayoutEstimation>
+      <div className="min-h-[calc((100vh-7rem))] w-full flex mx-auto flex-col justify-center md:flex-row md:max-w-screen-2xl md:py-8">
+        {/* Left Container */}
+        <ProjectStepContainer />
+        {/* Right Container */}
+        <MultitCardsContainer />
+        {/* Bottom Container */}
+        <BottomStepContainer />
+      </div>
+    </LayoutEstimation>
+  );
 };
 
 export default EstimationImmobiliere;
