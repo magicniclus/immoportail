29bb1b8775369099c6edf53cace436e593524959
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-13 13:21:33 +0100

    Update ProjectStepContainer.js

--- components/Organisms/ProjectStepContainer.js
+++ components/Organisms/ProjectStepContainer.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import TitleSecondary from '../Atoms/titles/TitleSecondary';
-import GetAdressInMap from '../Molecules/GetAdressInMap';
-import GetProjectProgress from '../Molecules/GetProjectProgress';
+import React from "react";
+import TitleSecondary from "../Atoms/titles/TitleSecondary";
+import GetAdressInMap from "../Molecules/GetAdressInMap";
+import GetProjectProgress from "../Molecules/GetProjectProgress";
 
 const ProjectStepContainer = () => {
-    return (
-        <div className="md:w-5/12  h-full mx-h-[850px] md:flex hidden bg-grayLight flex-col lg:pl-0 lg:items-center justify-between pl-5 py-10">
-            <TitleSecondary text="Caractéristiques de " textWithColor="votre bien" color="purple"/>
-            <GetProjectProgress />
-            <GetAdressInMap />
-        </div>
-    );
+  return (
+    <div className=" w-3/12  h-full max-h-[850px] md:flex hidden bg-grayLight flex-col lg:items-start justify-between pl-5 py-10">
+      <TitleSecondary
+        text="Caractéristiques de "
+        textWithColor="votre bien"
+        color="purple"
+      />
+      <GetProjectProgress />
+      <GetAdressInMap />
+    </div>
+  );
 };
 
 export default ProjectStepContainer;
