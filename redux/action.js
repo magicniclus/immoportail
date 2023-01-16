179c9c44ccde2f42673061c4a0800cf58d3db14b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-16 15:33:29 +0100

    Update action.js

--- redux/action.js
+++ redux/action.js
@@ -1,20 +1,27 @@
-export const updateAddressIsValid = (bool)=>{
-    return {
-        type: "updateAddressIsValid",
-        payload: bool
-    }
-}
+export const updateAddressIsValid = (bool) => {
+  return {
+    type: "updateAddressIsValid",
+    payload: bool,
+  };
+};
 
-export const updateAddress = (address)=>{
-    return {
-        type: "updateAddress",
-        payload: address
-    }
-}
+export const updateAddress = (address) => {
+  return {
+    type: "updateAddress",
+    payload: address,
+  };
+};
 
-export const updateAddressCoordinate = (coordinate)=>{
-    return {
-        type: "updateAddressCoordinate",
-        payload: coordinate
-    }
-}
+export const updateAddressCoordinate = (coordinate) => {
+  return {
+    type: "updateAddressCoordinate",
+    payload: coordinate,
+  };
+};
+
+export const updateStepOfProjectProgress = (stepValue) => {
+  return {
+    type: "stepOfProjectProgress",
+    payload: stepValue,
+  };
+};
