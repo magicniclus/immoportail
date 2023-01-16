9834aaf5213e271d028937c438f14d12551ee253
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-16 15:33:25 +0100

    Create WhatAddress.js

---
+++ components/Molecules/steps/WhatAddress.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SetInputWithTitleAndButton from "../SetInputWithTitleAndButton";
+
+const WhatAddress = () => {
+  return (
+    <div className="">
+      <SetInputWithTitleAndButton titleColor="purple" />
+    </div>
+  );
+};
+
+export default WhatAddress;
