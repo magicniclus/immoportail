2f7723fa554d2e4c1b06bcebae34ce20bb1e272a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-16 16:41:13 +0100

    Update WhatAddress.js

--- components/Molecules/steps/WhatAddress.js
+++ components/Molecules/steps/WhatAddress.js
@@ -1,10 +1,19 @@
 import React from "react";
+import InputWithLogo from "../../Atoms/inputs/InputWithLogo";
+import TitleSecondary from "../../Atoms/titles/TitleSecondary";
 import SetInputWithTitleAndButton from "../SetInputWithTitleAndButton";
 
 const WhatAddress = () => {
   return (
     <div className="">
-      <SetInputWithTitleAndButton titleColor="purple" />
+      <TitleSecondary text="Adresse du bien à estimer" color="purple" />
+      <InputWithLogo
+        componentIsForAdress={true}
+        image="/Vectorlocalization.png"
+        marginY="my-4"
+        placeholder="Saisir une adresse"
+        color="purple"
+      />
     </div>
   );
 };
