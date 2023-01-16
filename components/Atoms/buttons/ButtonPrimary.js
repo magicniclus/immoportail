cc9d9b6f9880437657ff737c466c5c8b76934804
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-16 16:41:06 +0100

    Update ButtonPrimary.js

--- components/Atoms/buttons/ButtonPrimary.js
+++ components/Atoms/buttons/ButtonPrimary.js
@@ -1,22 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const ButtonPrimary = (props) => {
-    const text = props.text
-    const version = props.version
-    const callback = props.callback
-    const disabled = props.disabled || false
-    const type = props.type || "button"
+  const text = props.text;
+  const version = props.version;
+  const callback = props.callback;
+  const disabled = props.disabled || false;
+  const type = props.type || "button";
+  const updateClass = props.updateClass;
 
-    const classDesign = {
-        primary: disabled ? "text-base px-10 py-2 bg-purpleLight rounded-3xl text-white hover:shadow-md hover:shadow-slate-200 transition-shadow": "text-base px-10 py-2 bg-purple rounded-3xl text-white hover:shadow-md hover:shadow-slate-200 transition-shadow",
-        secondary: disabled ? "text-base px-10 py-2 bg-newGray rounded-3xl text-purple hover:shadow-md hover:shadow-slate-200 transition-shadow":"text-base px-10 py-2 bg-white rounded-3xl text-purple hover:shadow-md hover:shadow-slate-200 transition-shadow"
-    }
+  const classDesign = {
+    primary: disabled
+      ? "text-base px-10 py-2 bg-purpleLight rounded-3xl text-white hover:shadow-md hover:shadow-slate-200 transition-shadow max-w-[150px] " +
+        updateClass
+      : "text-base px-10 py-2 bg-purple rounded-3xl text-white hover:shadow-md hover:shadow-slate-200 transition-shadow max-w-[150px] " +
+        updateClass,
+    secondary: disabled
+      ? "text-base px-10 py-2 bg-newGray rounded-3xl text-purple hover:shadow-md hover:shadow-slate-200 transition-shadow max-w-[150px] " +
+        updateClass
+      : "text-base px-10 py-2 bg-white rounded-3xl text-purple hover:shadow-md hover:shadow-slate-200 transition-shadow max-w-[150px] " +
+        updateClass,
+  };
 
-    return (
-        <button type={type} disabled={disabled} className={version !== undefined && version !== null ? classDesign[version] : classDesign.primary}>
-            {text}
-        </button>
-    );
+  return (
+    <button
+      type={type}
+      onClick={(e) => (callback ? callback(e) : null)}
+      disabled={disabled}
+      className={
+        version !== undefined && version !== null
+          ? classDesign[version]
+          : classDesign.primary
+      }
+    >
+      {text}
+    </button>
+  );
 };
 
 export default ButtonPrimary;
