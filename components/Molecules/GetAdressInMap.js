7e03c0af9e8980f6e082d7c2434c82a17972a01a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-17 15:08:48 +0100

    Update GetAdressInMap.js

--- components/Molecules/GetAdressInMap.js
+++ components/Molecules/GetAdressInMap.js
@@ -34,7 +34,7 @@ const GetAdressInMap = (props) => {
 
   // Utilisation d'un effet pour mettre à jour l'état de chargement du script lorsque les coordonnées de l'adresse changent
   useEffect(() => {
-    if (state.address !== "") {
+    if (state.address !== "" && state.addressCoordinate !== null) {
       setScriptLoaded(true);
     }
   }, [state.addressCoordinate]);
