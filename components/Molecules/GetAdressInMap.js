15b7dc532b9fca54809e2014c12c7aad7688637a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-17 13:45:15 +0100

    Update GetAdressInMap.js

--- components/Molecules/GetAdressInMap.js
+++ components/Molecules/GetAdressInMap.js
@@ -3,16 +3,22 @@ import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
 import { useSelector } from "react-redux";
 
 const GetAdressInMap = (props) => {
+  // Image personnalisée pour le marqueur
   const customMarker = "/vector/localizationHouse.png";
 
+  // Etat pour savoir si le script a été chargé
   const [scriptLoaded, setScriptLoaded] = useState(false);
+
+  // Accès à l'état global de l'application
   const state = useSelector((state) => state);
 
+  // Style pour le conteneur de la carte
   const containerStyle = {
     width: "100%",
     height: "100%",
   };
 
+  // Centre de la carte, basé sur les coordonnées de l'adresse
   const center = {
     lat:
       state.addressCoordinate !== null ? state.addressCoordinate.lat : 48.8566,
@@ -20,20 +26,21 @@ const GetAdressInMap = (props) => {
       state.addressCoordinate !== null ? state.addressCoordinate.lng : 2.3522,
   };
 
+  // Options pour la carte
   const options = {
     disableDefaultUI: true,
     zoomControl: false,
   };
 
+  // Utilisation d'un effet pour mettre à jour l'état de chargement du script lorsque les coordonnées de l'adresse changent
   useEffect(() => {
     if (state.address !== "") {
-      console.log(state.addressCoordinate);
       setScriptLoaded(true);
     }
   }, [state.addressCoordinate]);
 
+  // Fonction qui renvoie la carte de chargement
   const getLoading = () => {
-    console.log("ok");
     return (
       <LoadScript googleMapsApiKey="AIzaSyBhFIY1nvseuxoi4xA0HPiM-PvwNQdx9kI">
         <GoogleMap
@@ -50,8 +57,8 @@ const GetAdressInMap = (props) => {
     );
   };
 
+  // Fonction qui renvoie la carte
   const getComponent = () => {
-    console.log("ok deux");
     return (
       <GoogleMap
         id="map"
@@ -66,6 +73,7 @@ const GetAdressInMap = (props) => {
     );
   };
 
+  // Affiche soit la carte de chargement soit la carte elle-même en fonction de l'état de chargement
   return (
     <div className="h-48 w-11/12 bg-white shadow-[inset_0px_0px_15px_30px_#FAFAFA]">
       {!scriptLoaded ? getLoading() : getComponent()}
