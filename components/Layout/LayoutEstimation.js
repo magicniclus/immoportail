d45a4013f4d7382132850d349aed5184abf426af
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-17 17:02:42 +0100

    Update LayoutEstimation.js

--- components/Layout/LayoutEstimation.js
+++ components/Layout/LayoutEstimation.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head";
 import Footer from "../Organisms/Footer";
-import HeaderEstimation from '../Organisms/headers/HeaderEstimation';
+import HeaderEstimation from "../Organisms/headers/HeaderEstimation";
 
 const LayoutEstimation = (props) => {
-    return (
-        <>
-            <Head>
-                <title>{props.title}</title>
-                <meta property="og:title" content={props.description} key={props.key !== undefined ? props.key : "title"} />
-            </Head>
-            <header className=' mx-auto'>
-                <HeaderEstimation />
-            </header>
-            <main className="flex flex-col item-center relative min-h-[calc(100vh-7rem)] h-max mx-auto drop-shadow-md w-full">
-                {props.children}
-            </main>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>{props.title}</title>
+        <meta
+          property="og:title"
+          content={props.description}
+          key={props.key !== undefined ? props.key : "title"}
+        />
+      </Head>
+      <header className=" mx-auto">
+        <HeaderEstimation />
+      </header>
+      <main className="flex flex-col item-center relative min-h-[calc(100vh-7rem)] h-max mx-auto drop-shadow-md w-full">
+        {props.children}
+      </main>
+      {/* <Footer /> */}
+    </>
+  );
 };
 
 export default LayoutEstimation;
