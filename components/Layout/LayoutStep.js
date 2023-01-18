41fe9867faa627f6fb1873e6b7765935f544152a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-18 19:11:02 +0100

    Update LayoutStep.js

--- components/Layout/LayoutStep.js
+++ components/Layout/LayoutStep.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LayoutStep = (props) => {
-  return <div className="w-full mb-5 flex flex-col">{props.children}</div>;
+  return <div className="w-full mb-8 flex flex-col">{props.children}</div>;
 };
 
 export default LayoutStep;
