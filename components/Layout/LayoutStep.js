c42422f8661ae9f3d0324830245dad1f9bb26b47
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-19 10:29:27 +0100

    Update LayoutStep.js

--- components/Layout/LayoutStep.js
+++ components/Layout/LayoutStep.js
@@ -1,7 +1,14 @@
 import React from "react";
+import TitleSecondary from "../Atoms/titles/TitleSecondary";
 
 const LayoutStep = (props) => {
-  return <div className="w-full mb-8 flex flex-col">{props.children}</div>;
+  const title = props.title;
+  return (
+    <div className="w-full mb-8 flex flex-col">
+      <TitleSecondary text={title} color="purple" />
+      <div className="flex mt-5 flex-col">{props.children}</div>
+    </div>
+  );
 };
 
 export default LayoutStep;
