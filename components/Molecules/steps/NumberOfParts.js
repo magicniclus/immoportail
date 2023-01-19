e54395eefbb63444041af5bc314ff849a0136cb9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-19 10:29:29 +0100

    Update NumberOfParts.js

--- components/Molecules/steps/NumberOfParts.js
+++ components/Molecules/steps/NumberOfParts.js
@@ -1,29 +1,26 @@
 import React from "react";
 import SelectNumber from "../../Atoms/select/SelectNumber";
 import Text from "../../Atoms/texts/Text";
-import TitleSecondary from "../../Atoms/titles/TitleSecondary";
 import LayoutStep from "../../Layout/LayoutStep";
 
 const NumberOfParts = () => {
   return (
-    <LayoutStep>
-      <TitleSecondary
-        text="Combien de pièce comporte votre bien ? "
-        color="purple"
-      />
-      <div className="flex mt-5 flex-col">
-        <div className="">
-          <div className="mb-2">
-            <Text children="Nombre de pièce" color="purple" textSize="big" />
-          </div>
-          <SelectNumber />
+    <LayoutStep title="Combien de pièce comporte votre bien ?">
+      <div className="">
+        <div className="mb-2">
+          <Text children="Nombre de pièce" color="purple" textSize="bigLight" />
         </div>
-        <div className="mt-5">
-          <div className="mb-2">
-            <Text children="Nombre de chambre" color="purple" textSize="big" />
-          </div>
-          <SelectNumber />
+        <SelectNumber />
+      </div>
+      <div className="mt-5">
+        <div className="mb-2">
+          <Text
+            children="Nombre de chambre"
+            color="purple"
+            textSize="bigLight"
+          />
         </div>
+        <SelectNumber />
       </div>
     </LayoutStep>
   );
