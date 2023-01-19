e5551e94b036e302697d9ea519ccba1c377da6e8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-19 10:29:32 +0100

    Update WhatAccommodationType.js

--- components/Molecules/steps/WhatAccommodationType.js
+++ components/Molecules/steps/WhatAccommodationType.js
@@ -5,27 +5,21 @@ import CheckWithImage from "../cards/CheckWithImage";
 
 const WhatAccommodationType = () => {
   return (
-    <LayoutStep>
-      <div className="flex flex-col">
-        <TitleSecondary
-          text="Quel type de bien souhaitez-vous estimer ? "
-          color="purple"
+    <LayoutStep title="Quel type de bien souhaitez-vous estimer ?">
+      <div className="flex mt-2">
+        <CheckWithImage
+          updateStyle={"mr-10"}
+          imageAlt="maison"
+          image="house"
+          imageSize={{ width: 65, height: 48 }}
+          text="Maison"
         />
-        <div className="flex mt-5">
-          <CheckWithImage
-            updateStyle={"mr-10"}
-            imageAlt="maison"
-            image="house"
-            imageSize={{ width: 65, height: 48 }}
-            text="Maison"
-          />
-          <CheckWithImage
-            image="immeuble"
-            imageAlt="immeuble"
-            imageSize={{ width: 42, height: 55 }}
-            text="Immeuble"
-          />
-        </div>
+        <CheckWithImage
+          image="immeuble"
+          imageAlt="immeuble"
+          imageSize={{ width: 42, height: 55 }}
+          text="Immeuble"
+        />
       </div>
     </LayoutStep>
   );
