0717acc409c374a5bc3e205174baa76cb9d70563
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-19 19:27:19 +0100

    Update CheckWithImage.js

--- components/Molecules/cards/CheckWithImage.js
+++ components/Molecules/cards/CheckWithImage.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Text from "../../Atoms/texts/Text";
 
 const CheckWithImage = (props) => {
@@ -11,12 +11,32 @@ const CheckWithImage = (props) => {
   const imageAlt = props.imageAlt;
   const text = props.text;
 
+  const valueCallback = props.valueCallback;
+  const callback = props.callback;
+  const value = props.value;
+
+  useEffect(() => {
+    if (valueCallback !== value) setIsValid(false);
+  }, [valueCallback]);
+
+  const handleValid = (e) => {
+    e.preventDefault();
+    if (isValid) {
+      setIsValid(false);
+      callback(null);
+    }
+    if (!isValid) {
+      setIsValid(true);
+      callback(value);
+    }
+  };
+
   const afterValidButton =
     "after:content-[''] after:h-2 after:w-2 md:after:w-4 md:after:h-4 after:absolute after:rounded-full after:bg-purple after:top-1/2 after:left-1/2 after:transform after:-translate-x-1/2 after:-translate-y-1/2";
 
   return (
     <div
-      onClick={(e) => setIsValid(!isValid)}
+      onClick={handleValid}
       className={`w-28 h-28 border border-purple rounded-lg relative flex flex-col items-center justify-around p-3 cursor-pointer ${updateStyle}`}
     >
       <div
