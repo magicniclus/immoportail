2dc24fa0cf2de4e3690a6127b06d6d1f680f95a8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-19 17:23:29 +0100

    Update TitleSecondary.js

--- components/Atoms/titles/TitleSecondary.js
+++ components/Atoms/titles/TitleSecondary.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React from "react";
 
 const TitleSecondary = (props) => {
-    const text = props.text
-    const textWithColor = props.textWithColor
-    const color = props.color
-    const marginY = props.marginY
- 
-    const classDesign = {
-        white: "text-white text-base md:text-2xl "+ marginY ,
-        purple: "text-purple text-base md:text-2xl "+ marginY 
-    }
+  const text = props.text;
+  const textWithColor = props.textWithColor;
+  const color = props.color;
+  const marginY = props.marginY;
 
-    return (
-        <>
-            {
-                textWithColor !== undefined && textWithColor !== null ? 
-                <h2 className={color !== undefined && color !== null ?  classDesign[color] : classDesign.white}>{text}&nbsp;<span className='text-blue'>{textWithColor}</span></h2>:
-                <h2 className={color !== undefined && color !== null ?  classDesign[color] : classDesign.white}>{text}</h2>
-            }
-        </>
-    );
+  const classDesign = {
+    white: "text-white text-normal md:text-2xl " + marginY,
+    purple: "text-purple text-normal md:text-2xl " + marginY,
+  };
+
+  return (
+    <>
+      {textWithColor !== undefined && textWithColor !== null ? (
+        <h2
+          className={
+            color !== undefined && color !== null
+              ? classDesign[color]
+              : classDesign.white
+          }
+        >
+          {text}&nbsp;<span className="text-blue">{textWithColor}</span>
+        </h2>
+      ) : (
+        <h2
+          className={
+            color !== undefined && color !== null
+              ? classDesign[color]
+              : classDesign.white
+          }
+        >
+          {text}
+        </h2>
+      )}
+    </>
+  );
 };
 
 export default TitleSecondary;
