774b5bb2c8e4733c6cf2ee4f85d8d86dcd16a62b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-19 16:32:59 +0100

    Update SelectValue.js

--- components/Atoms/select/SelectValue.js
+++ components/Atoms/select/SelectValue.js
@@ -4,15 +4,16 @@ const SelectValue = (props) => {
   const option = props.option;
   return (
     <select
-      className="border min-w-[200px] max-w-[450px] rounded-lg placeholder-gray-200::placeholder px-3 h-12 border-purple text-purple"
+      className="text-xs md:text-sm border min-w-[200px] max-w-[450px] rounded-lg placeholder-gray-200::placeholder px-3 h-12 border-purple text-purple"
       name="type"
-      id="type"
     >
       <option disabled selected>
         Selectionnez une option
       </option>
       {option.map((el, idx) => (
-        <option key={idx}>{el}</option>
+        <option className="text-normal" key={idx}>
+          {el}
+        </option>
       ))}
     </select>
   );
