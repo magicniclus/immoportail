ef3d69983550c12ce6a69294590d130531825ff2
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-19 19:27:22 +0100

    Update WhatAccommodationType.js

--- components/Molecules/steps/WhatAccommodationType.js
+++ components/Molecules/steps/WhatAccommodationType.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LayoutStep from "../../Layout/LayoutStep";
 import CheckWithImage from "../cards/CheckWithImage";
+import { useState } from "react";
 
-const WhatAccommodationType = () => {
+const WhatAccommodationType = (props) => {
+  const isValid = props.isValid;
+
+  const [aCardIsSelected, setACardIsSelected] = useState(null);
+
+  useEffect(() => {
+    if (isValid) {
+      if (aCardIsSelected !== null) isValid(true);
+      else isValid(false);
+    }
+  }, [aCardIsSelected]);
+
   return (
     <LayoutStep title="Quel type de bien souhaitez-vous estimer ?">
       <div className="flex mt-2">
@@ -12,12 +24,18 @@ const WhatAccommodationType = () => {
           image="house"
           imageSize={{ width: 65, height: 48 }}
           text="Maison"
+          valueCallback={aCardIsSelected}
+          callback={setACardIsSelected}
+          value={1}
         />
         <CheckWithImage
           image="immeuble"
           imageAlt="immeuble"
           imageSize={{ width: 42, height: 55 }}
           text="Immeuble"
+          valueCallback={aCardIsSelected}
+          callback={setACardIsSelected}
+          value={2}
         />
       </div>
     </LayoutStep>
