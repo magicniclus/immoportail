c07fb033c32dbaea4bad0f9c68d12b1718e388a0
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-20 11:58:56 +0100

    Update WhatIsASurface.js

--- components/Molecules/steps/WhatIsASurface.js
+++ components/Molecules/steps/WhatIsASurface.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Input from "../../Atoms/inputs/Input";
 import Text from "../../Atoms/texts/Text";
 import LayoutStep from "../../Layout/LayoutStep";
+import { useDispatch } from "react-redux";
+import { updateASurface } from "../../../redux/action";
 
 const WhatIsASurface = () => {
+  const dispatch = useDispatch();
+
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    if (value !== "") dispatch(updateASurface(value));
+    if (value === "") dispatch(updateASurface(null));
+  }, [value]);
+
   return (
     <LayoutStep title="Quelle est la surface de votre bien ?">
       <div className="mb-2">
         <Text textSize="bigLight" color="purple" children="Valeur en m²" />
       </div>
-      <Input placeholder="Exemple: 100m²" type="number" />
+      <Input
+        placeholder="Exemple: 100m²"
+        type="number"
+        callback={setValue}
+        callbackValue={value}
+      />
     </LayoutStep>
   );
 };
