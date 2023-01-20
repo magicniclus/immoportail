2fadb7f59a8f27fe33deac2857d1a6582ec831e9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-20 12:55:51 +0100

    Update Check.js

--- components/Molecules/cards/Check.js
+++ components/Molecules/cards/Check.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Text from "../../Atoms/texts/Text";
 
 const Check = (props) => {
@@ -7,12 +7,32 @@ const Check = (props) => {
   const title = props.title;
   const updateStyle = props.updateStyle;
 
+  const valueCallback = props.valueCallback;
+  const callback = props.callback;
+  const value = props.value;
+
+  useEffect(() => {
+    if (valueCallback !== title) setIsValid(false);
+  }, [valueCallback]);
+
+  const handleValid = (e) => {
+    e.preventDefault();
+    if (isValid) {
+      setIsValid(false);
+      callback(null);
+    }
+    if (!isValid) {
+      setIsValid(true);
+      callback(title);
+    }
+  };
+
   const afterValidButton =
     "after:content-[''] after:h-2 after:w-2 md:after:w-4 md:after:h-4 after:absolute after:rounded-full after:bg-purple after:top-1/2 after:left-1/2 after:transform after:-translate-x-1/2 after:-translate-y-1/2";
 
   return (
     <div
-      onClick={(e) => setIsValid(!isValid)}
+      onClick={handleValid}
       className={`w-20 h-14 md:w-32 md:h-28 border border-purple rounded-lg relative flex flex-col items-center justify-around p-3 cursor-pointer ${updateStyle}`}
     >
       <div
