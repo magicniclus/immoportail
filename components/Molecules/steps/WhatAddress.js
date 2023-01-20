9376d134eff6b4ffff48709d1ec34e3d547ac927
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-20 17:53:29 +0100

    Update WhatAddress.js

--- components/Molecules/steps/WhatAddress.js
+++ components/Molecules/steps/WhatAddress.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import InputWithLogo from "../../Atoms/inputs/InputWithLogo";
 import LayoutStep from "../../Layout/LayoutStep";
 
 const WhatAddress = () => {
+  const state = useSelector((state) => state);
   return (
     <LayoutStep title="Adresse du bien à estimer ?">
       <InputWithLogo
