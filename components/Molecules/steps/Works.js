6688a516e6f7413ae891f6f304434f0e86622484
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-20 12:55:54 +0100

    Update Works.js

--- components/Molecules/steps/Works.js
+++ components/Molecules/steps/Works.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LayoutStep from "../../Layout/LayoutStep";
 import Check from "../cards/Check";
+import { useDispatch } from "react-redux";
+import { updateWorks } from "../../../redux/action";
 
-const Works = () => {
+const Works = (props) => {
+  const dispatch = useDispatch();
+
+  const isValid = props.isValid;
+
+  const [aCardIsSelected, setACardIsSelected] = useState(null);
+
+  useEffect(() => {
+    dispatch(updateWorks(aCardIsSelected));
+  }, [aCardIsSelected]);
+
+  useEffect(() => {
+    if (isValid) {
+      if (aCardIsSelected !== null) isValid(true);
+      else isValid(false);
+    }
+  }, [aCardIsSelected]);
+
   return (
     <LayoutStep title="Y'a t'il des travaux à prévoir dans votre bien ?">
       <div className="flex mt-2">
-        <Check title="Oui" updateStyle={"mr-5 md:mr-10"} />
-        <Check title="Non" />
+        <Check
+          title="Oui"
+          updateStyle={"mr-5 md:mr-10"}
+          valueCallback={aCardIsSelected}
+          callback={setACardIsSelected}
+          value={1}
+        />
+        <Check
+          title="Non"
+          valueCallback={aCardIsSelected}
+          callback={setACardIsSelected}
+          value={1}
+        />
       </div>
     </LayoutStep>
   );
