0ac01209cb3979934489fcf18a20f964fb0dda4a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-20 17:53:34 +0100

    Update Works.js

--- components/Molecules/steps/Works.js
+++ components/Molecules/steps/Works.js
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
 import LayoutStep from "../../Layout/LayoutStep";
 import Check from "../cards/Check";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { updateWorks } from "../../../redux/action";
 
 const Works = (props) => {
   const dispatch = useDispatch();
+  const state = useSelector((state) => state);
 
   const isValid = props.isValid;
 
   const [aCardIsSelected, setACardIsSelected] = useState(null);
+  const [type, setType] = useState("bien");
+
+  useEffect(() => {
+    if (state.estimationElements.accommodation !== null)
+      setType(state.estimationElements.accommodation);
+    else setType("bien");
+  }, [state.estimationElements.accommodation]);
 
   useEffect(() => {
     dispatch(updateWorks(aCardIsSelected));
@@ -23,7 +31,7 @@ const Works = (props) => {
   }, [aCardIsSelected]);
 
   return (
-    <LayoutStep title="Y'a t'il des travaux à prévoir dans votre bien ?">
+    <LayoutStep title={`Y'a t'il des travaux à prévoir dans votre ${type} ?`}>
       <div className="flex mt-2">
         <Check
           title="Oui"
