b28c2bade5960c5785b7c8a57095d01fd0b0d528
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-20 14:03:38 +0100

    Update ImportantSurfaces.js

--- components/Molecules/steps/ImportantSurfaces.js
+++ components/Molecules/steps/ImportantSurfaces.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LayoutStep from "../../Layout/LayoutStep";
 import Text from "../../Atoms/texts/Text";
 import Input from "../../Atoms/inputs/Input";
+import { useDispatch } from "react-redux";
+import { updateLivingArea, updateLandArea } from "../../../redux/action";
 
 const ImportantSurfaces = () => {
+  const dispatch = useDispatch();
+
+  const [livingAreaValue, setLivingAreaValue] = useState(null);
+  const [landAreaValue, setLandAreaValue] = useState(null);
+
+  useEffect(() => {
+    if (livingAreaValue !== null) dispatch(updateLivingArea(livingAreaValue));
+    else dispatch(updateLivingArea(null));
+    if (landAreaValue !== null) dispatch(updateLandArea(landAreaValue));
+    else dispatch(updateLandArea(null));
+  }, [livingAreaValue, landAreaValue]);
+
   return (
     <LayoutStep title="Quels est la surface de votre séjour et de votre bâti ?">
       <div className="">
@@ -14,7 +28,12 @@ const ImportantSurfaces = () => {
             textSize="bigLight"
           />
         </div>
-        <Input placeholder="Exemple: 100" type="number" />
+        <Input
+          placeholder="Exemple: 100"
+          type="number"
+          callback={setLivingAreaValue}
+          callbackValue={livingAreaValue}
+        />
       </div>
       <div className="mt-5">
         <div className="mb-2">
@@ -24,7 +43,12 @@ const ImportantSurfaces = () => {
             textSize="bigLight"
           />
         </div>
-        <Input placeholder="Exemple: 370" type="number" />
+        <Input
+          placeholder="Exemple: 370"
+          type="number"
+          callback={setLandAreaValue}
+          callbackValue={landAreaValue}
+        />
       </div>
     </LayoutStep>
   );
