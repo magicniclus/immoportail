959ff10bfe2943a13f7ecde9898a33a916772dde
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-20 12:55:48 +0100

    Update SelectValue.js

--- components/Atoms/select/SelectValue.js
+++ components/Atoms/select/SelectValue.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const SelectValue = (props) => {
   const option = props.option;
+
+  const [value, setValue] = useState("");
+
+  const valueCallback = props.valueCallback;
+  const callback = props.callback;
+
+  useEffect(() => {
+    if (callback) {
+      if (value > "") {
+        callback(value);
+      } else callback("");
+    }
+  }, [value]);
+
   return (
     <select
       className="text-xs md:text-sm border min-w-[200px] max-w-[450px] rounded-lg placeholder-gray-200::placeholder px-3 h-12 border-purple text-purple"
       name="type"
+      onChange={(e) => {
+        setValue(e.target.value);
+      }}
     >
       <option disabled selected>
         Selectionnez une option
