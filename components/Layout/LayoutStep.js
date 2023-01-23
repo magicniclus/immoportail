4587a842ae8a6228b723895b19bff7a3930f6935
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-23 12:04:08 +0100

    Update LayoutStep.js

--- components/Layout/LayoutStep.js
+++ components/Layout/LayoutStep.js
@@ -1,12 +1,20 @@
 import React from "react";
 import SeparationLign from "../Atoms/others/SeparationLign";
+import Text from "../Atoms/texts/Text";
 import TitleSecondary from "../Atoms/titles/TitleSecondary";
 
 const LayoutStep = (props) => {
   const title = props.title;
+  const indication = props.indication || null;
   return (
     <div className="w-full mb-8 flex flex-col">
       <TitleSecondary text={title} color="purple" />
+      <Text
+        textSize="bigLight"
+        color="purple"
+        children={indication}
+        marginX="font-thin"
+      />
       <SeparationLign color="yellow" marginY="mt-3" />
       <div className="flex mt-5 flex-col">{props.children}</div>
     </div>
