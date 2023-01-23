f8c2e45d84811185c48b7eb05704a48fe7f78924
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-23 14:32:51 +0100

    Update Exposure.js

--- components/Molecules/steps/Exposure.js
+++ components/Molecules/steps/Exposure.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import LayoutStep from "../../Layout/LayoutStep";
 import Text from "../../Atoms/texts/Text";
 import SelectValue from "../../Atoms/select/SelectValue";
+import Check from "../../Molecules/cards/Check";
 import { useDispatch, useSelector } from "react-redux";
 import { updateExposure } from "../../../redux/action";
 
@@ -9,8 +10,14 @@ const Exposure = () => {
   const exposure = ["Nord", "Sud", "Est", "Ouest"];
   const view = ["Exceptionnelle", "Dégagé", "Vis-à-vis"];
 
-  const [exposureValue, setExposureValue] = useState(null);
+  const [nord, setNord] = useState(false);
+  const [sud, setSud] = useState(false);
+  const [est, setEst] = useState(false);
+  const [ouest, setOuest] = useState(false);
+
   const [viewValue, setViewValue] = useState(null);
+
+  const exposureValue = [];
 
   const dispatch = useDispatch();
 
@@ -25,29 +32,45 @@ const Exposure = () => {
   useEffect(() => {
     dispatch(
       updateExposure({
-        exposure: exposureValue,
+        exposure: [
+          nord ? nord : null,
+          sud ? sud : null,
+          est ? est : null,
+          ouest ? ouest : null,
+        ],
         view: viewValue,
       })
     );
-  }, [exposureValue, viewValue]);
+  }, [viewValue, nord, sud, est, ouest]);
+
   return (
-    <LayoutStep
-      title="Exposition/vue ?* "
-      indication="Plusieurs choix possibles"
-    >
+    <LayoutStep title="Exposition/vue ?* ">
       <div className="mb-2">
         <Text
-          children={`Quelle est l'exposition de votre ${type} ?`}
+          children={`Quelle est l'exposition de votre ${type} (Plusieurs choix possibles) ?`}
           color="purple"
           textSize="bigLight"
         />
       </div>
-      <div className="flex">
-        <SelectValue
-          option={exposure}
-          callback={setExposureValue}
-          callbackValue={exposureValue}
-        />
+      <div className="flex flex-col">
+        <div className="flex">
+          <Check
+            title="Nord"
+            updateStyle={"mr-5 md:mr-10"}
+            valueCallback={nord}
+            callback={setNord}
+          />
+          <Check title="Sud" valueCallback={sud} callback={setSud} />
+        </div>
+        <div className="flex mt-5">
+          <Check
+            title="Est"
+            valueCallback={est}
+            callback={setEst}
+            updateStyle={"mr-5 md:mr-10"}
+          />
+          <Check title="Ouest" valueCallback={ouest} callback={setOuest} />
+        </div>
       </div>
       <div className="mt-5 mb-2">
         <Text
