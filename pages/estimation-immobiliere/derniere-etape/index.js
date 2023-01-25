76f7805b90f01e713b10f92f53360005231c1f1f
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-25 16:24:28 +0100

    Update index.js

--- pages/estimation-immobiliere/derniere-etape/index.js
+++ pages/estimation-immobiliere/derniere-etape/index.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import LayoutEstimation from "../../../components/Layout/LayoutEstimation";
 import Inscription from "../../../components/Molecules/form/Inscription";
+import { updateStepOfProjectProgress } from "../../../redux/action";
 
 const index = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(updateStepOfProjectProgress("lastStep"));
+  }, []);
   return (
     <LayoutEstimation>
       <Inscription />
