9bd8f6013ed66ca03340430e93f3b3f6fb399423
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-25 15:50:12 +0100

    Create InputText.js

---
+++ components/Atoms/inputs/InputText.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+const inputText = (props) => {
+  const placeholder = props.placeholder;
+  const color = props.color;
+  const type = props.type;
+  const minLenght = props.minLenght;
+  const required = props.required;
+
+  const [value, setValue] = useState("");
+
+  const valueCallback = props.valueCallback;
+  const callback = props.callback;
+
+  useEffect(() => {
+    if (callback) {
+      if (value !== "") {
+        callback(value);
+      } else callback(null);
+    }
+  }, [value]);
+
+  return (
+    <input
+      className={`border min-w-[200px] rounded-lg placeholder-gray-200::placeholder px-3  max-w-[450px] h-12 border-purple text-xs md:text-sm text-ellipsis overflow-hidden whitespace-nowrap text-purple`}
+      required={required}
+      placeholder={placeholder}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      type={type ? type : null}
+    />
+  );
+};
+
+export default inputText;
