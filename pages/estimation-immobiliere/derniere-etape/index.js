ba59726596c463fc04a55721f72a0e5236026713
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-25 15:50:28 +0100

    Create index.js

---
+++ pages/estimation-immobiliere/derniere-etape/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LayoutEstimation from "../../../components/Layout/LayoutEstimation";
+import Inscription from "../../../components/Molecules/form/Inscription";
+
+const index = () => {
+  return (
+    <LayoutEstimation>
+      <Inscription />
+    </LayoutEstimation>
+  );
+};
+
+export default index;
