0464ab3323b32cfe4f0475bf448d6603a12d9665
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-25 15:50:18 +0100

    Update TitlePrimary.js

--- components/Atoms/titles/TitlePrimary.js
+++ components/Atoms/titles/TitlePrimary.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React from "react";
 
 const TitlePrimary = (props) => {
-    const text = props.text;
-    const textWithColor = props.textWithColor
-    const color = props.color
+  const text = props.text;
+  const textWithColor = props.textWithColor;
+  const color = props.color;
+  const updateWidth = props.updateWidth || null;
 
-    const classDesign = {
-        white:"text-white text-2xl md:text-4xl font-bold md:w-10/12",
-        purple:"text-purple text-2xl md:text-4xl font-bold md:w-10/12"
-    }
+  const classDesign = {
+    white: `text-white text-2xl md:text-4xl font-bold ${
+      updateWidth !== null ? updateWidth : "md:w-10/12"
+    }`,
+    purple: `text-purple text-2xl md:text-4xl font-bold ${
+      updateWidth !== null ? updateWidth : "md:w-10/12"
+    }`,
+  };
 
-    return (
-        <>
-            {
-                textWithColor !== undefined && textWithColor !== null ? 
-                <h1 className={color !== undefined ? classDesign[color] : classDesign.white}>{text}&nbsp;<span className='text-blue'>{textWithColor}</span></h1>:
-                <h1 className={color !== undefined ? classDesign[color] : classDesign.white}>{text}</h1>
-            }
-        </>
-    );
+  return (
+    <>
+      {textWithColor !== undefined && textWithColor !== null ? (
+        <h1
+          className={
+            color !== undefined ? classDesign[color] : classDesign.white
+          }
+        >
+          {text}&nbsp;<span className="text-blue">{textWithColor}</span>
+        </h1>
+      ) : (
+        <h1
+          className={
+            color !== undefined ? classDesign[color] : classDesign.white
+          }
+        >
+          {text}
+        </h1>
+      )}
+    </>
+  );
 };
 
 export default TitlePrimary;
