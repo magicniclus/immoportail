f662d4238e1ae76a52def7b7611e220b3ad467cf
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-26 12:10:18 +0100

    Create firebase.config.js

---
+++ pages/api/firebase/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBhFIY1nvseuxoi4xA0HPiM-PvwNQdx9kI",
+  authDomain: "immo-portail.firebaseapp.com",
+  projectId: "immo-portail",
+  storageBucket: "immo-portail.appspot.com",
+  messagingSenderId: "903030755314",
+  appId: "1:903030755314:web:c67816803b3d2a3519256b",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const dataBase = getFirestore(app);
+export const auth = getAuth(app);
