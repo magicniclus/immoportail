2de9268938659652382b104114c830637bfc270c
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-26 16:13:48 +0100

    Update Estimation.js

--- pages/api/homadata/Estimation.js
+++ pages/api/homadata/Estimation.js
@@ -2,14 +2,18 @@ import { mockedData } from "./MockedData";
 
 const mocked = true;
 
+const updateMockedData = () => {
+  return Promise.resolve(mockedData);
+};
+
 export const getEstimation = (object) => {
   return new Promise((resolve, reject) => {
     if (mocked) {
-      mockedData()
+      updateMockedData()
         .then((value) => resolve(value))
         .catch((err) => {
           reject(err);
         });
-    }
+    } else null;
   });
 };
