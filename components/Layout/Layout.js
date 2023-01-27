721df32d810d82b08138e3155051cf648bf05ab5
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-27 08:04:15 +0100

    Update Layout.js

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,21 +1,39 @@
 import Header from "../Organisms/headers/Header";
-import Head from 'next/head'
+import Head from "next/head";
 import Footer from "../Organisms/Footer";
+import { useSelector } from "react-redux";
+import Loader from "../Organisms/Loader";
+import React, { useEffect, useState } from "react";
 
-export default function Layout(props){
-    return (
-        <>
-            <Head>
-                <title>{props.title}</title>
-                <meta property="og:title" content={props.description} key={props.key !== undefined ? props.key : "title"} />
-            </Head>
-            <header>
-                <Header />
-            </header>
-            <main className="relative">
-                {props.children}
-            </main>
-            <Footer />
-        </>
-    )
+export default function Layout(props) {
+  const state = useSelector((state) => state);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (state.loader) setLoading(true);
+    else setLoading(false);
+  }, [state.loader]);
+
+  useEffect(() => {
+    if (state.loader) setLoading(true);
+    else setLoading(false);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>{props.title}</title>
+        <meta
+          property="og:title"
+          content={props.description}
+          key={props.key !== undefined ? props.key : "title"}
+        />
+      </Head>
+      <header>
+        <Header />
+      </header>
+      <main className="relative">{loading ? <Loader /> : props.children}</main>
+      <Footer />
+    </>
+  );
 }
