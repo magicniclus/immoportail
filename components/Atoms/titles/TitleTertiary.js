f18bc570ed6cce310303970a8502c577c95868e7
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-27 15:14:51 +0100

    Update TitleTertiary.js

--- components/Atoms/titles/TitleTertiary.js
+++ components/Atoms/titles/TitleTertiary.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React from "react";
 
 const TitleTertiary = (props) => {
-     const text = props.text;
-    const textWithColor = props.textWithColor
-    const color = props.color;
-    const marginY = props.marginY
+  const text = props.text;
+  const textWithColor = props.textWithColor;
+  const color = props.color;
+  const marginY = props.marginY;
 
-    const classDesign = {
-        white: "text-white text-base md:text-3xl text-center "+marginY,
-        purple: "text-purple text-base md:text-3xl text-center "+marginY
-    }
+  const classDesign = {
+    white: "text-white text-base md:text-2xl text-center " + marginY,
+    purple: "text-purple text-base md:text-2xl text-center " + marginY,
+  };
 
-    return (
-        <>
-            {
-                textWithColor !== undefined && textWithColor !== null ? 
-                <h2 className={color !== undefined && color !== null ?  classDesign[color] : classDesign.white}>{text}&nbsp;<span className='text-blue'>{textWithColor}</span></h2>:
-                <h2 className={color !== undefined && color !== null ?  classDesign[color] : classDesign.white}>{text}</h2>
-            }
-        </>
-    );
+  return (
+    <>
+      {textWithColor !== undefined && textWithColor !== null ? (
+        <h3
+          className={
+            color !== undefined && color !== null
+              ? classDesign[color]
+              : classDesign.white
+          }
+        >
+          {text}&nbsp;<span className="text-blue">{textWithColor}</span>
+        </h3>
+      ) : (
+        <h3
+          className={
+            color !== undefined && color !== null
+              ? classDesign[color]
+              : classDesign.white
+          }
+        >
+          {text}
+        </h3>
+      )}
+    </>
+  );
 };
 
 export default TitleTertiary;
