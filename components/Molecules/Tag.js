fd108b2faab96af36ebe675b56151a51d2f01ac8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-27 16:07:36 +0100

    Update Tag.js

--- components/Molecules/Tag.js
+++ components/Molecules/Tag.js
@@ -4,7 +4,7 @@ import Text from "../Atoms/texts/Text";
 const Tag = (props) => {
   const text = props.text || null;
   return (
-    <div className="bg-purple rounded-lg py-1 px-2 h-min w-fit">
+    <div className="bg-purple rounded-lg py-1 px-2 h-min w-fit mb-1">
       <Text
         textSize="titleCard"
         color="white"
