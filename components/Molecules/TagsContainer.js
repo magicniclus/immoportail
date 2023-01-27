01ffab3b504cd8259d170fbacbec8750a5f0b307
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-27 16:07:39 +0100

    Update TagsContainer.js

--- components/Molecules/TagsContainer.js
+++ components/Molecules/TagsContainer.js
@@ -3,8 +3,11 @@ import Tag from "./Tag";
 
 const TagsContainer = (props) => {
   const tags = props.tags || null;
+  const updateStyle = props.updateStyle || null;
   return (
-    <div className="flex">
+    <div
+      className={`flex w-[50%] max-w-[400px] flex-wrap justify-around ${updateStyle}`}
+    >
       {tags.map((item, idx) => (
         <Tag key={idx} text={item} />
       ))}
