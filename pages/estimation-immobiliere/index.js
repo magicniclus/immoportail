a09ab91a0d96c5757cc4088c740068afbd08bd9b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-30 10:14:59 +0100

    Update index.js

--- pages/estimation-immobiliere/index.js
+++ pages/estimation-immobiliere/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import LayoutEstimation from "../../components/Layout/LayoutEstimation";
 import BottomStepContainer from "../../components/Organisms/BottomStepContainer";
 import MultitCardsContainer from "../../components/Organisms/MultitCardsContainer";
 import ProjectStepContainer from "../../components/Organisms/ProjectStepContainer";
+import { updateStepOfProjectProgress } from "../../redux/action";
 
 const EstimationImmobiliere = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    dispatch(updateStepOfProjectProgress(1));
+  }, []);
   return (
     <LayoutEstimation>
       <div className="min-h-[calc((100vh-7rem))] w-full flex mx-auto flex-col justify-center md:flex-row md:max-w-screen-2xl md:py-8">
