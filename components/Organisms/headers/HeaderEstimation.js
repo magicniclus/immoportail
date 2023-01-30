98de8a696915722e9064f2ce1c4a465614717333
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-30 10:14:50 +0100

    Update HeaderEstimation.js

--- components/Organisms/headers/HeaderEstimation.js
+++ components/Organisms/headers/HeaderEstimation.js
@@ -1,23 +1,29 @@
-import Link from 'next/link';
-import React from 'react';
-import Logo from '../../Atoms/logos/Logo';
-import TitleSecondary from '../../Atoms/titles/TitleSecondary';
+import Link from "next/link";
+import React from "react";
+import Logo from "../../Atoms/logos/Logo";
+import TitleSecondary from "../../Atoms/titles/TitleSecondary";
+import TitleTertiary from "../../Atoms/titles/TitleTertiary";
 
 const HeaderEstimation = () => {
-    return (
-        <div className='w-screen z-20 bg-white h-28 drop-shadow-md flex items-center px-5 lg:px-40'>
-            <div className=" items-baseline lg:flex-row w-full lg:flex flex-col mx-auto ">
-                {/* Left container */}
-                <Link href="/">
-                    <Logo />
-                </Link>
-                {/* Right container */}
-                <div className="lg:ml-5">
-                    <TitleSecondary text="Votre estimation en ligne en " textWithColor="2 minutes" color="purple"/>
-                </div>
-            </div>
+  return (
+    <div className="w-screen z-20 bg-white h-28 drop-shadow-md flex items-center px-5 lg:px-40">
+      <div className=" items-baseline lg:flex-row w-full lg:flex flex-col mx-auto ">
+        {/* Left container */}
+        <Link href="/">
+          <Logo />
+        </Link>
+        {/* Right container */}
+        <div className="lg:ml-5 flex justify-start">
+          <TitleTertiary
+            text="Votre estimation en ligne en "
+            textWithColor="2 minutes"
+            color="purple"
+            marginY="font-light text-left"
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default HeaderEstimation;
