133ea09a5ee7e7af4d66bf64038c84d03dde16d9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-30 16:16:25 +0100

    Update ButtonWithLogoAndText.js

--- components/Atoms/buttons/ButtonWithLogoAndText.js
+++ components/Atoms/buttons/ButtonWithLogoAndText.js
@@ -1,35 +1,54 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const ButtonWithLogoAndText = (props) => {
-    const text = props.text
-    const image = props.image
-    const after = props.after
-    const size = props.size
+  const text = props.text;
+  const image = props.image;
+  const after = props.after;
+  const size = props.size;
+  const link = props.link || "/";
 
-    const classDesign = {
-        small:{
-            withAfter: 'group/button w-32 relative flex items-center flex-col after:content-[""] after:absolute after:right-0 after:bottom-0 after:w-px after:h-full after:bg-purple',
-            withoutAfter:'group/button w-32 relative flex items-center flex-col'
-        },
-        normal:{
-            withAfter: 'group/button w-36 relative flex items-center flex-col after:content-[""] after:absolute after:right-0 after:bottom-0 after:w-px after:h-full after:bg-purple',
-            withoutAfter:'group/button w-36 relative flex items-center flex-col'
-        },
-        big:{
-            withAfter: 'group/button w-48 relative flex items-center flex-col after:content-[""] after:absolute after:right-0 after:bottom-0 after:w-px after:h-full after:bg-purple',
-            withoutAfter:'group/button w-48 relative flex items-center flex-col'
-        }
-    }
+  const classDesign = {
+    small: {
+      withAfter:
+        'group/button w-32 relative flex items-center flex-col after:content-[""] after:absolute after:right-0 after:bottom-0 after:w-px after:h-full after:bg-purple',
+      withoutAfter: "group/button w-32 relative flex items-center flex-col",
+    },
+    normal: {
+      withAfter:
+        'group/button w-36 relative flex items-center flex-col after:content-[""] after:absolute after:right-0 after:bottom-0 after:w-px after:h-full after:bg-purple',
+      withoutAfter: "group/button w-36 relative flex items-center flex-col",
+    },
+    big: {
+      withAfter:
+        'group/button w-48 relative flex items-center flex-col after:content-[""] after:absolute after:right-0 after:bottom-0 after:w-px after:h-full after:bg-purple',
+      withoutAfter: "group/button w-48 relative flex items-center flex-col",
+    },
+  };
 
-    return (
-        <button className={size===undefined ? classDesign.normal.withAfter : classDesign[size][after]}>
-            <div className="w-7 relative h-8  group-hover/button:scale-105  transition-transform">
-                <Image src={image === undefined ? "/VectorLight.png" : image} layout='fill' objectFit='contain' alt="icon" />
-            </div>
-            <p className="mx-3 text-purple">{text === undefined ? "Recommandations" : text}</p>
-        </button>
-    );
+  return (
+    <Link
+      href={link}
+      className={
+        size === undefined
+          ? classDesign.normal.withAfter
+          : classDesign[size][after]
+      }
+    >
+      <div className="w-7 relative h-8  group-hover/button:scale-105  transition-transform">
+        <Image
+          src={image === undefined ? "/VectorLight.png" : image}
+          layout="fill"
+          objectFit="contain"
+          alt="icon"
+        />
+      </div>
+      <p className="mx-3 text-purple">
+        {text === undefined ? "Recommandations" : text}
+      </p>
+    </Link>
+  );
 };
 
 export default ButtonWithLogoAndText;
