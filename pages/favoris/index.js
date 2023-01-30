21f5082a91b360b8c327629658ad3f4e49663f9a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-30 15:13:17 +0100

    Update index.js

--- pages/favoris/index.js
+++ pages/favoris/index.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Layout from "../../components/Layout/Layout";
+import TitlePrimary from "../../components/Atoms/titles/TitlePrimary";
+import { useRouter } from "next/router";
 
 const index = () => {
-  // TODO : Create the page
-  return <div></div>;
+  const route = useRouter();
+  useEffect(() => {
+    route.push("/connexion");
+  }, []);
+
+  return (
+    <Layout>
+      <TitlePrimary text="Vos Favoris" />
+    </Layout>
+  );
 };
 
 export default index;
