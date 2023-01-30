0d506b146e3d4cfb406e21de72544ba1c3f9823b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-30 16:08:58 +0100

    Create index.js

---
+++ pages/unsubscribe/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+
+const index = () => {
+  //TODO : Create the page
+  return (
+    <Layout>
+      <h1>Unsubscribe</h1>
+    </Layout>
+  );
+};
+
+export default index;
