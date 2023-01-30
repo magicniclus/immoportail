39bf6eb8bca03db2ac891226693e6393a0aee2fb
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-30 14:15:16 +0100

    Create CookieBanner.js

---
+++ components/Organisms/CookieBanner.js
@@ -0,0 +1,67 @@
+import { useRouter } from "next/router";
+import React, { useState, useEffect } from "react";
+
+export const CookieBanner = () => {
+  const router = useRouter();
+
+  const [accepted, setAccepted] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const acceptedFromStorage =
+        localStorage.getItem("acceptedCookies") === "true";
+      setAccepted(acceptedFromStorage);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const acceptedFromStorage =
+        localStorage.getItem("acceptedCookies") === "true";
+      setAccepted(acceptedFromStorage);
+    }
+  }, [typeof window]);
+
+  const handleAccept = () => {
+    setAccepted(true);
+    if (typeof window !== "undefined" && window.localStorage) {
+      localStorage.setItem("acceptedCookies", true);
+    }
+  };
+
+  const handleReject = () => {
+    setAccepted(false);
+    router.push("google.com");
+  };
+
+  if (accepted) return null;
+
+  return (
+    <div
+      className={`fixed bottom-0 right-0 w-full bg-gray-400 text-white px-10 py-20 z-50 ${
+        typeof window !== "undefined" &&
+        window.localStorage &&
+        localStorage.getItem("acceptedCookies") !== null &&
+        localStorage.getItem("acceptedCookies")
+          ? "hidden"
+          : "block"
+      }`}
+    >
+      <p>
+        Ce site utilise des cookies pour améliorer votre expérience. En
+        continuant à naviguer sur ce site, vous acceptez notre utilisation des
+        cookies.
+      </p>
+      <div className="mt-5">
+        <button className="text-blue font-semibold mr-5" onClick={handleAccept}>
+          J'accepte
+        </button>
+        <button className="text-gray-100 font-light" onClick={handleReject}>
+          Je refuse
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CookieBanner;
