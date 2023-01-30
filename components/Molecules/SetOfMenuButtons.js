36b6b06f0d3f24a955e4a52a1750e6e9cbf211d9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-30 16:16:30 +0100

    Update SetOfMenuButtons.js

--- components/Molecules/SetOfMenuButtons.js
+++ components/Molecules/SetOfMenuButtons.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import ButtonWithLogo from '../Atoms/buttons/ButtonWithLogo';
-import ButtonWithLogoAndText from '../Atoms/buttons/ButtonWithLogoAndText';
+import React from "react";
+import ButtonWithLogo from "../Atoms/buttons/ButtonWithLogo";
+import ButtonWithLogoAndText from "../Atoms/buttons/ButtonWithLogoAndText";
 
 const SetOfMenuButtons = () => {
-    return (
-        <div>
-            <div className="hidden lg:flex">
-                {/* Button one */}
-                <ButtonWithLogoAndText image="/VectorLight.png" text="Recommandations" size="big" after="withAfter"/>
-                {/* Button two */}
-                <ButtonWithLogoAndText image="/VectorLove.png" text="Favoris" size="normal" after="withAfter" />
-                {/* Button three */}
-                <ButtonWithLogo image="/Vectoruser.png" />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="hidden lg:flex">
+        {/* Button one */}
+        <ButtonWithLogoAndText
+          image="/VectorLight.png"
+          text="Recommandations"
+          size="big"
+          after="withAfter"
+          link="/recommandations"
+        />
+        {/* Button two */}
+        <ButtonWithLogoAndText
+          image="/VectorLove.png"
+          text="Favoris"
+          size="normal"
+          after="withAfter"
+          link="/favoris"
+        />
+        {/* Button three */}
+        <ButtonWithLogo image="/Vectoruser.png" link="/connexion" />
+      </div>
+    </div>
+  );
 };
 
 export default SetOfMenuButtons;
