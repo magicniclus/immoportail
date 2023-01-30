4d9e8dda3e35ef6736870d90d168ef8216778f4c
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-30 15:03:02 +0100

    Create index.js

---
+++ pages/connexion/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Connexion from "../../components/Molecules/form/Connexion";
+import Layout from "../../components/Layout/Layout";
+
+const index = () => {
+  return (
+    <Layout>
+      <Connexion />
+    </Layout>
+  );
+};
+
+export default index;
