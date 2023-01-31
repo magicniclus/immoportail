77e46f6185bc085d9702e2ae5e2e71dc05b58b6a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-31 18:10:36 +0100

    Update SetTitleWithDescriptionAndMiddleSeparation.js

--- components/Molecules/SetTitleWithDescriptionAndMiddleSeparation.js
+++ components/Molecules/SetTitleWithDescriptionAndMiddleSeparation.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import SeparationLign from '../Atoms/others/SeparationLign';
-import TitlePrimary from "../Atoms/titles/TitlePrimary"
-import Text from "../Atoms/texts/Text"
+import React from "react";
+import SeparationLign from "../Atoms/others/SeparationLign";
+import TitlePrimary from "../Atoms/titles/TitlePrimary";
+import Text from "../Atoms/texts/Text";
 
 const SetTitleWithDescriptionAndMiddleSeparation = (props) => {
-    const title = props.title
-    const titleWithColor = props.titleWithColor
-    const colorTitle = props.colorTitle
-    const colorSeparation = props.colorSeparation
-    return (
-        <div className=" lg:w-7/12">
-            <TitlePrimary text={title} textWithColor={titleWithColor} color={colorTitle} />
-            <SeparationLign color={colorSeparation} marginY="my-4"/>
-            <Text textSize="normal" color="purple">
-                La valeur d’un bien immobilier est estimé d’abord par son emplacement, en effet si le bien est situé dans une rue calme en dehors du centre ville, ou en campagne ou alors dans un lieu plus vivant avec plus de passage, le prix peut grandement varier. <br/><br/>
-
-                C’est pour cela que nous conseillons à tout propriétaire aillant pour projet de vendre de prendre rendez-vous avec un conseiller spécialisé afin d’évaluer, d’affiner ou de conseiller afin de rentre le projet le plus précis possible. 
-            </Text>
-        </div>
-    );
+  const title = props.title;
+  const titleWithColor = props.titleWithColor;
+  const colorTitle = props.colorTitle;
+  const colorSeparation = props.colorSeparation;
+  return (
+    <div className=" lg:w-7/12">
+      <TitlePrimary
+        text={title}
+        textWithColor={titleWithColor}
+        color={colorTitle}
+      />
+      <SeparationLign color={colorSeparation} marginY="my-4" />
+      <Text textSize="normal" color="purple">
+        La localisation joue un rôle primordial dans l'évaluation de la valeur
+        d'un bien immobilier. Que ce soit dans une rue paisible à l'extérieur du
+        centre-ville, en campagne ou dans un endroit animé avec plus de passage,
+        la localisation peut fortement influencer le prix.
+        <br />
+        <br /> C'est pourquoi nous recommandons à tout propriétaire ayant
+        l'intention de vendre de prendre rendez-vous avec l'un de nos
+        conseillers immobiliers spécialisés pour évaluer, affiner et conseiller
+        sur leur projet de vente afin d'obtenir les estimations les plus
+        précises possible.
+      </Text>
+    </div>
+  );
 };
 
 export default SetTitleWithDescriptionAndMiddleSeparation;
