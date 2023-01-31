e6185a704d8c090835c9ce827a427502b5651bd0
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-31 11:12:59 +0100

    Update index.js

--- pages/unsubscribe/index.js
+++ pages/unsubscribe/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import UnsubscribeBanner from "../../components/Organisms/UnsubscribeBanner";
 
 const index = () => {
   //TODO : Create the page
   return (
     <Layout>
-      <h1>Unsubscribe</h1>
+      <UnsubscribeBanner />
     </Layout>
   );
 };
