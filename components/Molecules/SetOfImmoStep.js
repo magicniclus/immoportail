a411115d08c747316933b3250df4a0c01215cef9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-31 18:10:32 +0100

    Update SetOfImmoStep.js

--- components/Molecules/SetOfImmoStep.js
+++ components/Molecules/SetOfImmoStep.js
@@ -1,47 +1,97 @@
-import React from 'react';
-import Text from '../Atoms/texts/Text';
-import TitlePrimary from '../Atoms/titles/TitlePrimary';
-import SeparationLign from '../Atoms/others/SeparationLign';
-import TitleSecondary from '../Atoms/titles/TitleSecondary';
-import SetOfAdvice from './SetOfAdvice';
+import React from "react";
+import Text from "../Atoms/texts/Text";
+import TitlePrimary from "../Atoms/titles/TitlePrimary";
+import SeparationLign from "../Atoms/others/SeparationLign";
+import TitleSecondary from "../Atoms/titles/TitleSecondary";
+import SetOfAdvice from "./SetOfAdvice";
 
 const SetOfImmoStep = (props) => {
-    const textForAdvice = props.textForAdvice
-    const colorForAdvice = props.colorForAdvice
+  const textForAdvice = props.textForAdvice;
+  const colorForAdvice = props.colorForAdvice;
 
-    return (
-        <div>
-            <TitlePrimary text="Quel sont les étapes " textWithColor="d’un projet immobilier ?" color="purple" />
-            <SeparationLign color="yellow" marginY="my-4"/>
-            <div className="">
-                <div className="my-10">
-                    <TitleSecondary marginY="mb-3" text="1. Estimer correctement votre bien" color="purple" />
-                    <Text textSize="light" color="purple">
-                        L’étape la plus importante, et afin d’affiner votre projet, n’hésitez pas à prendre rendez-vous avec un de nos conseiller qui vous apportera toutes les clés à la réussite de votre projet.
-                    </Text>
-                </div>
-                <div className="my-10">
-                    <TitleSecondary marginY="mb-3" text="2. Mettre en vente son bien" color="purple" />
-                    <Text textSize="light" color="purple">
-                        Il faut mettre en ventre son bien au bon prix, pour cela il existe un multitude de plateforme, mais également le réseau interne d’un professionnel. 
-                    </Text>
-                </div>
-                <SetOfAdvice size="long" title="Notre conseil" text="La diffusion d’un annonce peu couter très cher à un particulier si il veut que son annonce remonte en haut des listes de diffusion. C’est une des raisons pour cela qu’il est important de passer pas un professionnel qui as des partenariats avec la plupart d’entre elles. "/>
-                <div className="my-10">
-                    <TitleSecondary marginY="mb-3" text="3. Trouver le bon acheteur" color="purple" />
-                    <Text textSize="light" color="purple">
-                        La aussi il n’est pas nécessaire de réaliser énormément de visite ou d’accepter la première proposition venu. Il suffit d’un acquéreur ! Faite confiance à votre agent pour réaliser les différentes visite et filtrer les potentiel réel acquéreurs afin d’éviter un perte de temps inutile.   
-                    </Text>
-                </div>
-                <div className="my-10">
-                    <TitleSecondary marginY="mb-3" text="4. Passage à l’acte notarial " color="purple" />
-                    <Text textSize="light" color="purple">
-                        La aussi il n’est pas nécessaire de réaliser énormément de visite ou d’accepter la première proposition venu. Il suffit d’un acquéreur ! Faite confiance à votre agent pour réaliser les différentes visite et filtrer les potentiel réel acquéreurs afin d’éviter un perte de temps inutile.   
-                    </Text>
-                </div>
-            </div>
+  return (
+    <div>
+      <TitlePrimary
+        text="Quel sont les étapes "
+        textWithColor="d’un projet immobilier ?"
+        color="purple"
+      />
+      <SeparationLign color="yellow" marginY="my-4" />
+      <div className="">
+        <div className="my-10">
+          <TitleSecondary
+            marginY="mb-3"
+            text="1. Déterminer la valeur de votre bien"
+            color="purple"
+          />
+          <Text textSize="light" color="purple">
+            La première étape dans le processus de vente immobilière consiste à
+            déterminer la valeur de votre bien. Cela peut être fait en
+            consultant un agent immobilier ou en utilisant des outils en ligne
+            pour évaluer la valeur de votre propriété. L'évaluation de votre
+            bien est importante car cela déterminera le prix que vous pouvez en
+            demander. Une estimation précise peut également vous aider à éviter
+            de sur-estimer ou de sous-estimer votre bien.
+          </Text>
         </div>
-    );
+        <div className="my-10">
+          <TitleSecondary
+            marginY="mb-3"
+            text="2. Préparer votre bien pour la vente"
+            color="purple"
+          />
+          <Text textSize="light" color="purple">
+            Une fois la valeur de votre bien déterminée, la prochaine étape
+            consiste à le préparer pour la vente. Cela peut inclure la
+            réparation de tout défaut, la mise en valeur de votre bien en
+            améliorant son apparence et en mettant en évidence ses
+            caractéristiques les plus attrayantes. Les petits travaux de
+            rénovation peuvent faire une grande différence dans l'attrait de
+            votre bien pour les acheteurs potentiels. La mise en valeur de votre
+            bien peut également aider à maximiser le prix que vous pouvez en
+            obtenir.
+          </Text>
+        </div>
+        <SetOfAdvice
+          size="long"
+          title="Notre conseil"
+          text="Diffuser une annonce immobilière peut coûter cher pour un particulier qui cherche à la faire remonter en tête des résultats. Pour éviter cela, il est judicieux de faire appel à un professionnel doté de partenariats avec les plateformes les plus populaires. "
+        />
+        <div className="my-10">
+          <TitleSecondary
+            marginY="mb-3"
+            text="3. Trouver le bon acheteur"
+            color="purple"
+          />
+          <Text textSize="light" color="purple">
+            Une fois que votre bien est prêt pour la vente, la prochaine étape
+            consiste à trouver un acheteur potentiel. Passer par une agence
+            immobilière peut vous donner accès à un grand nombre de potentiels
+            acheteurs. Les agents immobiliers sont également en mesure de vous
+            aider à trouver les meilleurs acheteurs en fonction de vos objectifs
+            de vente et de la valeur de votre bien. Ils peuvent également gérer
+            les offres et les négociations pour vous.
+          </Text>
+        </div>
+        <div className="my-10">
+          <TitleSecondary
+            marginY="mb-3"
+            text="4. Finaliser la vente "
+            color="purple"
+          />
+          <Text textSize="light" color="purple">
+            La dernière étape consiste à finaliser la vente de votre bien. Cela
+            peut inclure la signature de contrats et la finalisation de toutes
+            les formalités administratives nécessaires. Les agents immobiliers
+            peuvent vous aider à naviguer à travers ce processus pour vous
+            assurer que la vente se déroule en douceur et sans encombre. Ils
+            peuvent également vous aider à gérer les problèmes qui peuvent
+            survenir au cours de ce processus
+          </Text>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SetOfImmoStep;
