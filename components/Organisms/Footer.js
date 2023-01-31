1b21ee78edb0f12ef80046c591346f78a3761d5a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-31 11:12:45 +0100

    Update Footer.js

--- components/Organisms/Footer.js
+++ components/Organisms/Footer.js
@@ -1,12 +1,55 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { emailRegex } from "../../lib/regex/Regex";
+import { useDispatch } from "react-redux";
 import Logo from "../Atoms/logos/Logo";
 import TitleSecondary from "../Atoms/titles/TitleSecondary";
 import InputWithLogo from "../Atoms/inputs/InputWithLogo";
 import ButtonPrimary from "../Atoms/buttons/ButtonPrimary";
+import { updateModal } from "../../redux/action";
+import { setNewsLettersUserDoc } from "../../pages/api/firebase/Doc";
+import { createId } from "../../lib/utils/func";
 
 const Footer = () => {
-  //TODO : add the link to the footer
+  const [disabled, setDisabled] = useState(true);
+  const [mail, setMail] = useState(null);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (emailRegex.test(mail)) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [mail]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(createId);
+    if (mail !== null) {
+      setNewsLettersUserDoc({ id: createId, mail: mail })
+        .then((success) => {
+          dispatch(
+            updateModal({
+              message:
+                "Nous sommes ravis de vous compter parmis nos membres, bienvenue !",
+              show: true,
+            })
+          );
+        })
+        .catch((error) => {
+          console.log(error);
+          dispatch(
+            updateModal({
+              message: "Une erreur est survenue, veuillez essayer à nouveau.",
+              show: true,
+            })
+          );
+        });
+    }
+  };
+
   return (
     <footer className="w-full py-20 px-5 lg:px-40 lg:flex lg:justify-between bg-gray-300 lg:flex-row flex-col drop-shadow-md ">
       <div className=" max-w-screen-2xl flex justify-between w-full lg:flex lg:justify-between lg:flex-row flex-col mx-auto">
@@ -31,7 +74,10 @@ const Footer = () => {
             © immoportail.fr 2022
           </p>
         </div>
-        <form className="flex flex-col justify-between items-start lg:items-end lg:w-4/12 w-8/12 lg:h-auto h-64 my-7 lg:m-0">
+        <form
+          className="flex flex-col justify-between items-start lg:items-end lg:w-4/12 w-8/12 lg:h-auto h-64 my-7 lg:m-0"
+          onSubmit={handleSubmit}
+        >
           <Logo />
           <TitleSecondary
             text="S’inscrire à notre news letters"
@@ -42,8 +88,15 @@ const Footer = () => {
             placeholder="Saisir un email"
             image="/vector/Vectorletter.png"
             color="white"
+            valueCallback={mail}
+            callback={setMail}
           />
-          <ButtonPrimary text="S'inscrire" version="primary" />
+          <ButtonPrimary
+            text="S'inscrire"
+            version="primary"
+            disabled={disabled}
+            type="submit"
+          />
         </form>
       </div>
     </footer>
