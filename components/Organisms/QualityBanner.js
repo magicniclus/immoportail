be2b78346823d64b2dfbf41ec390d38b81b32883
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-01-31 18:10:48 +0100

    Update QualityBanner.js

--- components/Organisms/QualityBanner.js
+++ components/Organisms/QualityBanner.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import SetOfQuality from '../Molecules/SetOfQuality';
+import React from "react";
+import SetOfQuality from "../Molecules/SetOfQuality";
 
 const QualityBanner = () => {
-    return (
-        <div className='w-full'>
-            <div className="flex-col lg:flex-row flex justify-between py-10 md:py-16 items-center px-5 md:px-40">
-                <SetOfQuality image="Vectorcheck" title="Estimation" titleWithColor="en ligne" text="Estimez votre bien en quelques clic grâce à notre outil d’estimation 100% gratuit, fiable, basé sur les données officiel mis à jour en temps réel." />
-                <SetOfQuality image="Vectoruserpurple" title="Un unique" titleWithColor="conseiller" text="Estimez votre bien en quelques clic grâce à notre outil d’estimation 100% gratuit, fiable, basé sur les données officiel mis à jour en temps réel." />
-                <SetOfQuality image="Vectorsafe" title="Protection de" titleWithColor="vos données" text="Estimez votre bien en quelques clic grâce à notre outil d’estimation 100% gratuit, fiable, basé sur les données officiel mis à jour en temps réel." />
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full">
+      <div className="flex-col lg:flex-row flex justify-between py-10 md:py-16 items-center px-5 md:px-40">
+        <SetOfQuality
+          image="Vectorcheck"
+          title="Estimation"
+          titleWithColor="en ligne"
+          text="Découvrez la valeur de votre bien immobilier avec notre outil d'estimation en ligne, entièrement gratuit et fiable. Basé sur les données du marché en temps réel, obtenez une estimation précise en quelques clics seulement."
+        />
+        <SetOfQuality
+          image="Vectoruserpurple"
+          title="Un unique"
+          titleWithColor="conseiller"
+          text="Un conseiller partenaire dédiés est spécialisée dans le secteur de l'immobilier pour vous offrir une expertise inégalée. Bénéficiez d'une estimation personnalisée en prenant rendez-vous avec l'un de nos conseillers spécialisés."
+        />
+        <SetOfQuality
+          image="Vectorsafe"
+          title="Protection de"
+          titleWithColor="vos données"
+          text="Vos données sont en sécurité avec nous! Nous utilisons les technologies de sécurité les plus avancées pour protéger vos informations personnelles. Confiez-nous votre estimation en toute confiance - nous garantissons la confidentialité de vos données."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default QualityBanner;
