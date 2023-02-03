dc3c67e473831bebbde10c80015d8384ccea13d8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-02-03 15:00:32 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_VITE_APP_GMAP_API_KEY:
+      process.env.NEXT_PUBLIC_VITE_APP_GMAP_API_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
