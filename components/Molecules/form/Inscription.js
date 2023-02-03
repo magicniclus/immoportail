7271b48eac0deac8b3560c42f15cc9ca9f39e9a4
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-02-03 15:00:09 +0100

    Update Inscription.js

--- components/Molecules/form/Inscription.js
+++ components/Molecules/form/Inscription.js
@@ -41,6 +41,7 @@ const Inscription = (props) => {
     else setDisabled(true);
   }, [firstName, lastName, phone, mail]);
 
+  // TODO: ajouter le lien pour annuler le stockage des données dans la politique de confidentialité
   return (
     <form
       className="mx-auto w-10/12 sm:w-4/6 md:w-2/6 flex flex-col py-10 md:py-10 my-auto max-w-[450px]"
