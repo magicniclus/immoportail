c2e7de2005c3c62d2a391689eeb832c1bb62e35b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-02-03 15:00:07 +0100

    Update Logo.js

--- components/Atoms/logos/Logo.js
+++ components/Atoms/logos/Logo.js
@@ -1,12 +1,19 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 const Logo = () => {
-    return (
-        <div className="cursor-pointer h-7 relative flex items-center w-40 md:w-56">
-            <Image priority src="/Portailimmologo.png" layout='fill' objectFit='contain' objectPosition='left' alt="logo ImmoPortail" />
-        </div>
-    );
+  return (
+    <div className="cursor-pointer h-7 relative flex items-center w-40 md:w-56">
+      <Image
+        priority
+        src="/logos/Immosurlogo.png"
+        layout="fill"
+        objectFit="contain"
+        objectPosition="left"
+        alt="logo ImmoPortail"
+      />
+    </div>
+  );
 };
 
 export default Logo;
