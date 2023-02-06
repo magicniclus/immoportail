9976c92f54df478b956494f453fb75a9d5b3e514
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-02-06 10:55:30 +0100

    Update index.js

--- pages/politique-de-confidentialite/index.js
+++ pages/politique-de-confidentialite/index.js
@@ -5,9 +5,9 @@ import Link from "next/link";
 const Index = () => {
   return (
     <Layout>
-      <div className="min-h-[calc((100vh-7rem))] w-full flex mx-auto flex-col justify-center md:max-w-screen-2xl md:py-5 px-10 item my-10">
+      <div className="min-h-[calc((100vh-7rem))] w-full flex mx-auto flex-col justify-center md:max-w-screen-2xl md:py-5 px-10 item my-10 text-purple">
         <h1 className="text-2xl mb-5 mx-auto">Politique de confidentialité</h1>
-        <h2 className="mt-5">2-PREAMBULE</h2>
+        <h2 className="mt-5">1-PREAMBULE</h2>
         <p className="mt-3">
           Cette politique de confidentialité décrit les pratiques de immo-sur.fr
           en matière de collecte, d&apos;utilisation, de conservation et de
@@ -19,9 +19,10 @@ const Index = () => {
         <h2 className="mt-5">2-DÉFINITIONS</h2>
         <p className="mt-3">
           Les termes &quot;vous&quot; et &quot;utilisateur&quot; désignent toute
-          personne qui accède, utilise ou consulte le site immo-sur.fr. Les
-          termes &quot;nous&quot;, &quot;notre&quot; et &quot;immo-sur.fr&quot;
-          désignent le propriétaire et exploitant du site.
+          personne qui accède, utilise ou consulte le site avenue-immo.com. Les
+          termes &quot;nous&quot;, &quot;notre&quot; et
+          &quot;avenue-immo.com&quot; désignent le propriétaire et exploitant du
+          site.
         </p>
         <h2 className="mt-5">
           3-CHAMP D&apos;APPLICATION, ACCEPTATION ET MODIFICATION DES CGU
@@ -29,10 +30,10 @@ const Index = () => {
         <p className="mt-3">
           Cette politique de confidentialité s&apos;applique à toutes les
           informations personnelles collectées ou soumises sur le site
-          immo-sur.fr. Nous nous réservons le droit de modifier cette politique
-          de temps à autre sans préavis. Votre utilisation continue du site
-          constitue votre acceptation de toutes les modifications apportées à
-          cette politique.
+          avenue-immo.com. Nous nous réservons le droit de modifier cette
+          politique de temps à autre sans préavis. Votre utilisation continue du
+          site constitue votre acceptation de toutes les modifications apportées
+          à cette politique.
         </p>
         <h2 className="mt-5">4-ACCES ET DISPONIBILITE DES SERVICES</h2>
         <p className="mt-3">
@@ -65,14 +66,14 @@ const Index = () => {
         <h2 className="mt-5">8-DISPOSITIONS DIVERSES</h2>
         <p className="mt-3">
           7.1. Cette politique de confidentialité constitue l&apos;intégralité
-          de l&apos;accord entre l&apos;utilisateur et immo-sûr.fr en ce qui
+          de l&apos;accord entre l&apos;utilisateur et avenue-immo.com en ce qui
           concerne l&apos;utilisation de ses services.
         </p>
         <p className="mt-3">
           7.2. Tout manquement à l&apos;une des obligations prévues dans cette
           politique de confidentialité peut entraîner la résiliation immédiate
           des services sans préjudice de tous dommages et intérêts auxquels
-          immo-sûr.fr pourrait prétendre.
+          avenue-immo.com pourrait prétendre.
         </p>
         <p className="mt-3">
           7.3. Si une disposition de cette politique de confidentialité est
@@ -81,16 +82,16 @@ const Index = () => {
           des dispositions restantes.
         </p>
         <p className="mt-3">
-          7.4. Immo-sûr.fr se réserve le droit de modifier cette politique de
-          confidentialité à tout moment en publiant une nouvelle version sur son
-          site web. Les utilisateurs sont tenus de se tenir informés de tout
+          7.4. avenue-immo.com se réserve le droit de modifier cette politique
+          de confidentialité à tout moment en publiant une nouvelle version sur
+          son site web. Les utilisateurs sont tenus de se tenir informés de tout
           changement en consultant régulièrement cette politique de
           confidentialité.
         </p>
         <h2 className="mt-5">10-DROIT APPLICABLE ET REGLEMENT DES LITIGES</h2>
         <p className="mt-3">
           8.1. Tout litige relatif à l&apos;utilisation des services
-          d&apos;immo-sûr.fr sera soumis au droit français.
+          d&apos;avenue-immo.com sera soumis au droit français.
         </p>
         <p className="mt-3">
           8.2. En cas de litige, les parties s&apos;efforceront de trouver une
@@ -101,13 +102,13 @@ const Index = () => {
           10-PROTECTION DES DONNEES A CARACTERE PERSONNEL
         </h2>
         <p className="mt-3">
-          9.1. Immo-sûr.fr s&apos;engage à protéger les données personnelles de
-          ses utilisateurs conformément à la réglementation applicable en
+          9.1. Avenue-immo.com s&apos;engage à protéger les données personnelles
+          de ses utilisateurs conformément à la réglementation applicable en
           matière de protection des données personnelles, notamment le Règlement
           Général sur la Protection des Données (RGPD).
         </p>
         <p className="mt-3">
-          9.2. Immo-sûr.fr ne collecte que les données nécessaires à
+          9.2. Avenue-immo.com ne collecte que les données nécessaires à
           l&apos;exécution de ses services. Les données collectées sont traitées
           de manière confidentielle et ne sont pas transmises à des tiers sans
           le consentement de l&apos;utilisateur.
@@ -119,7 +120,7 @@ const Index = () => {
           <Link href="/unsubscribe">ici</Link>.
         </p>
         <p className="mt-3">
-          9.4. Immo-sûr.fr prend les mesures de sécurité appropriées pour
+          9.4. Avenue-immo.com prend les mesures de sécurité appropriées pour
           protéger les données personnelles de ses utilisateurs contre tout
           accès non autorisé, toute modification, tout usage illicite ou toute
           perte accidentelle.
