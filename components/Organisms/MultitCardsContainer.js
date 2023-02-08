8e33d0d60c3a2d1c105641611e561c1d80d1a297
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-02-08 18:34:23 +0100

    Update MultitCardsContainer.js

--- components/Organisms/MultitCardsContainer.js
+++ components/Organisms/MultitCardsContainer.js
@@ -126,8 +126,8 @@ const MultitCardsContainer = () => {
 
   const updateAddress = async (idx) => {
     let baseUrl = "http://localhost:3005";
-    if (process.env.NEXT_PUBLIC_PORT) {
-      baseUrl = `https://avenue-immo.com:${process.env.NEXT_PUBLIC_PORT}`;
+    if (process.env.PORT) {
+      baseUrl = `https://avenue-immo.com:${process.env.PORT}`;
     }
     axios
       .get(`${baseUrl}/api/places/autocomplete?address=${address}`)
