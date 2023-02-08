b215e95ce4f4eabaf39d3c73c179d169a887ecab
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-02-08 18:24:07 +0100

    Update AdressForm.js

--- components/Molecules/form/AdressForm.js
+++ components/Molecules/form/AdressForm.js
@@ -42,7 +42,7 @@ const AdressForm = (props) => {
 
     let baseUrl = "http://localhost:3005";
     if (process.env.PORT) {
-      baseUrl = `https://avenue-immo.com:${process.env.PORT}`;
+      baseUrl = `https://avenue-immo.com:${process.env.NEXT_PUBLIC_PORT}`;
     }
 
     if (address.length < 3) {
