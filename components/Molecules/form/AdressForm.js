088d453cd1f97d45fcd4af52e511f27bcbacc0a1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-02-09 17:07:35 +0100

    Update AdressForm.js

--- components/Molecules/form/AdressForm.js
+++ components/Molecules/form/AdressForm.js
@@ -1,69 +1,44 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import axios from "axios";
+import { useState, useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { useDispatch, useSelector } from "react-redux";
 import { updateAddress } from "../../../redux/action";
-import { getAddressPrediction } from "../../../lib/googleMap/googleMap";
+import { useRouter } from "next/router";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
 
 const AdressForm = (props) => {
   const placeholder = props.placeholder;
   const color = props.color;
-  const [source, setSource] = useState(axios.CancelToken.source());
-
-  const API_KEY = "AIzaSyBhFIY1nvseuxoi4xA0HPiM-PvwNQdx9kI";
-
   const router = useRouter();
   const currentUrl = router.asPath;
-
-  const state = useSelector((state) => state);
-  // Déclarez un état local pour stocker l'adresse saisie par l'utilisateur
-  const [address, setAddress] = useState(
-    currentUrl === "/estimation-immobiliere" ? state.address : ""
-  );
-  // Déclarez un état local pour stocker les suggestions d'adresse
-  const [suggestions, setSuggestions] = useState([]);
-
-  const [suggestionsAreOpen, setSuggestionsAreOpen] = useState(false);
-
-  const dispatch = useDispatch();
-
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyBhFIY1nvseuxoi4xA0HPiM-PvwNQdx9kI",
+    libraries: ["places"],
+  });
+  const [selected, setSelected] = useState(null);
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
   const [isClicked, setIsClicked] = useState(
     currentUrl === "/estimation-immobiliere" ? true : false
   );
 
-  // Utilisez useEffect pour récupérer les suggestions d'adresse à mesure que l'utilisateur saisit l'adresse
-  useEffect(() => {
-    setSuggestionsAreOpen(false);
-    if (address.trim() === "") {
-      setSuggestions([]);
-      setSource(axios.CancelToken.source());
-      dispatch(updateAddress(""));
-      return;
-    }
+  const handleSelect = async (address) => {
+    clearSuggestions();
 
-    // let baseUrl = "http://localhost:3005";
-    // if (process.env.PORT) {
-    // let baseUrl = `https://avenue-immo.com:${process.env.PORT}`;
-    // }
-
-    if (address.length < 3) {
-      return;
-    } else {
-      axios
-        .get(
-          `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${address}&key=${API_KEY}&components=country:fr`
-        )
-        .then((res) => {
-          setSuggestions(res.data.predictions);
-        })
-        .catch((error) => {
-          console.error(error);
-          setSuggestions("Erreur, veuillez essayer à nouveau");
-        });
-    }
-  }, [address]);
+    const results = await getGeocode({ address });
+    const { lat, lng } = await getLatLng(results[0]);
+    setSelected({ lat, lng });
+  };
 
   const handleAddress = (description) => {
+    setValue(address, false);
     source.cancel(`Cancel request for ${address}`);
     setSuggestions([]);
     setAddress(description);
@@ -73,19 +48,11 @@ const AdressForm = (props) => {
     setIsClicked(true);
   };
 
-  useEffect(() => {
-    if (suggestions.length > 0) setSuggestionsAreOpen(true);
-    else setSuggestionsAreOpen(false);
-  }, [suggestions]);
-
   return (
     <div>
-      {/* Utilisez un champ de saisie pour saisir l'adresse */}
       <input
-        type="text"
-        value={address}
-        onChange={(e) => (!isClicked ? setAddress(e.target.value) : null)}
-        onClick={() => setIsClicked(false)}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
         className={
           color !== undefined
             ? "border text-purple rounded-lg placeholder:text-slate-400 px-3 w-full h-12 border-" +
@@ -94,23 +61,23 @@ const AdressForm = (props) => {
         }
         placeholder={placeholder !== undefined ? placeholder : null}
       />
-      {/* Affichez les suggestions d'adresse dans une liste */}
-      {!isClicked && suggestionsAreOpen ? (
-        <ul className={"bg-white py-1"}>
-          {Array.isArray(suggestions) &&
-            suggestions.map((suggestion, idx) => (
-              <li
-                key={idx}
-                onClick={() => handleAddress(suggestion.description)}
-                className="text-purple cursor-pointer mx-3 my-3 hover:font-bold z-50"
-              >
-                {suggestion.description}
-              </li>
-            ))}
-        </ul>
-      ) : null}
+      <div>
+        {!isClicked ? (
+          <ul className={"bg-white py-1"}>
+            {status === "OK" &&
+              data.map(({ place_id, description }) => (
+                <li
+                  onClick={() => setValue(description)}
+                  key={place_id}
+                  className="text-purple cursor-pointer mx-3 my-3 hover:font-bold z-50"
+                >
+                  {description}
+                </li>
+              ))}
+          </ul>
+        ) : null}
+      </div>
     </div>
   );
 };
-
 export default AdressForm;
