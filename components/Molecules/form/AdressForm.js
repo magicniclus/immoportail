48369393dd614c976337de352fcafba4bcbfe598
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-02-10 15:40:00 +0100

    Update AdressForm.js

--- components/Molecules/form/AdressForm.js
+++ components/Molecules/form/AdressForm.js
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { useDispatch, useSelector } from "react-redux";
 import { updateAddress } from "../../../redux/action";
@@ -13,70 +13,90 @@ const AdressForm = (props) => {
   const color = props.color;
   const router = useRouter();
   const currentUrl = router.asPath;
+  const dispatch = useDispatch();
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyBhFIY1nvseuxoi4xA0HPiM-PvwNQdx9kI",
     libraries: ["places"],
   });
+
   const [selected, setSelected] = useState(null);
+  const [isClicked, setIsClicked] = useState(
+    currentUrl === "/estimation-immobiliere" ? true : false
+  );
+
+  const [isMapVisible, setIsMapVisible] = useState(isLoaded);
+
+  useEffect(() => {
+    if (isLoaded) {
+      setIsMapVisible(true);
+    }
+  }, [isLoaded]);
+
   const {
     ready,
     value,
     setValue,
     suggestions: { status, data },
     clearSuggestions,
-  } = usePlacesAutocomplete();
-  const [isClicked, setIsClicked] = useState(
-    currentUrl === "/estimation-immobiliere" ? true : false
-  );
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      componentRestrictions: {
+        country: "fr",
+      },
+    },
+  });
 
   const handleSelect = async (address) => {
+    setValue(address, false);
     clearSuggestions();
 
-    const results = await getGeocode({ address });
-    const { lat, lng } = await getLatLng(results[0]);
-    setSelected({ lat, lng });
+    try {
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      setSelected({ lat, lng });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const handleAddress = (description) => {
-    setValue(address, false);
-    source.cancel(`Cancel request for ${address}`);
-    setSuggestions([]);
-    setAddress(description);
-    setSource(axios.CancelToken.source());
+    setValue(description);
     dispatch(updateAddress(description));
-    // dispatch(updateStepOfProjectProgress(1));
     setIsClicked(true);
   };
-
   return (
     <div>
-      <input
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        className={
-          color !== undefined
-            ? "border text-purple rounded-lg placeholder:text-slate-400 px-3 w-full h-12 border-" +
-              color
-            : "border text-purple rounded-lg placeholder:text-slate-400 px-3 w-full h-12 border-purple"
-        }
-        placeholder={placeholder !== undefined ? placeholder : null}
-      />
-      <div>
-        {!isClicked ? (
-          <ul className={"bg-white py-1"}>
-            {status === "OK" &&
-              data.map(({ place_id, description }) => (
-                <li
-                  onClick={() => setValue(description)}
-                  key={place_id}
-                  className="text-purple cursor-pointer mx-3 my-3 hover:font-bold z-50"
-                >
-                  {description}
-                </li>
-              ))}
-          </ul>
-        ) : null}
-      </div>
+      {isMapVisible ? (
+        <>
+          <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            className={
+              color !== undefined
+                ? "border text-purple rounded-lg placeholder:text-slate-400 px-3 w-full h-12 border-" +
+                  color
+                : "border text-purple rounded-lg placeholder:text-slate-400 px-3 w-full h-12 border-purple"
+            }
+            placeholder={placeholder !== undefined ? placeholder : null}
+          />
+          <div>
+            {!isClicked ? (
+              <ul className={"bg-white py-1"}>
+                {status === "OK" &&
+                  data.map(({ place_id, description }) => (
+                    <li
+                      onClick={() => setValue(description)}
+                      key={place_id}
+                      className="text-purple cursor-pointer mx-3 my-3 hover:font-bold z-50"
+                    >
+                      {description}
+                    </li>
+                  ))}
+              </ul>
+            ) : null}
+          </div>
+        </>
+      ) : null}
     </div>
   );
 };
