0dd1683c1f454c25f78624aa0365458464f7b793
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-02-15 15:05:34 +0100

    Update GetAdressInMap.js

--- components/Molecules/GetAdressInMap.js
+++ components/Molecules/GetAdressInMap.js
@@ -7,13 +7,25 @@ const KEY_API =
   "pk.eyJ1IjoibWFnaWNuaWNsdXMiLCJhIjoiY2w1cG13bDdnMW96YTNvcGp5a3hnanE4bCJ9.o82GL_7qj6RuhYkF84fTZw";
 
 const GetAddressInMap = () => {
-  const [viewState, setViewState] = React.useState({
+  const state = useSelector((state) => state);
+  const [viewState, setViewState] = useState({
     longitude: -0.57918,
     latitude: 44.837789,
-    zoom: 16,
+    zoom: 14,
     height: "80vh",
     width: "100%",
   });
+
+  useEffect(() => {
+    if (state.addressCoordinate) {
+      setViewState((prevState) => ({
+        ...prevState,
+        latitude: state.addressCoordinate.lat,
+        longitude: state.addressCoordinate.lng,
+      }));
+    }
+  }, [state.addressCoordinate]);
+
   return (
     <div className="h-42 w-11/12 bg-white shadow-[inset_0px_0px_15px_30px_#FAFAFA]">
       <ReactMapGL
