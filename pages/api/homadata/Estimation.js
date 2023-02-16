c28668aea69d28a3cdadac3eabfd8dc4489862a4
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-02-16 12:16:43 +0100

    Update Estimation.js

--- pages/api/homadata/Estimation.js
+++ pages/api/homadata/Estimation.js
@@ -33,21 +33,29 @@ const userData = {
 };
 
 // Configuration de la requête
-const apiKey = "c7e6b36c05049c7d57a18b2af71455839818fd4f"; // Remplacez YOUR_API_KEY par votre clé d'API réelle
-const apiDomain = "https://www.avenue-immo.com/"; // Remplacez YOUR_API_DOMAIN par votre domaine API réel
-const apiUrl = `https://bdvapis.appspot.com/estimation/v3.0.0/transaction`;
-
-const config = {
-  header: {
-    Authorization: `Bearer ${apiKey}`,
-    Origin: apiDomain,
-  },
-  params: {
-    userData,
-  },
-};
 
 export const getAllEstimation = () => {
+  const apiKey = "c7e6b36c05049c7d57a18b2af71455839818fd4f"; // Remplacez YOUR_API_KEY par votre clé d'API réelle
+  const apiDomain = "https://www.avenue-immo.com/"; // Remplacez YOUR_API_DOMAIN par votre domaine API réel
+  const apiUrl = `https://bdvapis.appspot.com/estimation/v3.0.0/transaction`;
+
+  const config = {
+    headers: {
+      Authorization: `Bearer c7e6b36c05049c7d57a18b2af71455839818fd4f`,
+      Origin: "https://www.avenue-immo.com/",
+    },
+    params: {
+      lon: 2.294481,
+      lat: 48.85837,
+      propertyType: 0,
+      propertySurface: 72,
+      roomNb: 3,
+      bedroomNb: 2,
+      floor: 3,
+      floorNb: 2,
+      gardenSurface: 0,
+    },
+  };
   return new Promise((resolve, reject) => {
     axios
       .get(apiUrl, config)
