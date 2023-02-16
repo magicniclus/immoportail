8ccdfea5be22cf10d4cedaef5b81e7f63e3758cf
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-02-16 11:37:43 +0100

    Update Estimation.js

--- pages/api/homadata/Estimation.js
+++ pages/api/homadata/Estimation.js
@@ -1,4 +1,5 @@
 import { mockedData } from "./MockedData";
+import axios from "axios";
 
 const mocked = true;
 
@@ -15,5 +16,47 @@ export const getEstimation = (object) => {
           reject(err);
         });
     } else null;
+  });
+};
+
+// Récupération des données de l'utilisateur
+const userData = {
+  lon: 2.294481,
+  lat: 48.85837,
+  propertyType: 0,
+  propertySurface: 72,
+  roomNb: 3,
+  bedroomNb: 2,
+  floor: 3,
+  floorNb: 2,
+  gardenSurface: 0,
+};
+
+// Configuration de la requête
+const apiKey = "c7e6b36c05049c7d57a18b2af71455839818fd4f"; // Remplacez YOUR_API_KEY par votre clé d'API réelle
+const apiDomain = "https://www.avenue-immo.com/"; // Remplacez YOUR_API_DOMAIN par votre domaine API réel
+const apiUrl = `https://bdvapis.appspot.com/estimation/v3.0.0/transaction`;
+
+const config = {
+  headers: {
+    Authorization: `Bearer ${apiKey}`,
+    Origin: apiDomain,
+  },
+  params: {
+    userData,
+  },
+};
+
+export const getAllEstimation = () => {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(apiUrl, userData, config)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((error) => {
+        console.error(error);
+        reject(error);
+      });
   });
 };
