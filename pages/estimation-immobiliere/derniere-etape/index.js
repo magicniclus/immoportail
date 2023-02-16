8faf044443bec974838c220e6dc72d9995e07715
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-02-16 11:37:45 +0100

    Update index.js

--- pages/estimation-immobiliere/derniere-etape/index.js
+++ pages/estimation-immobiliere/derniere-etape/index.js
@@ -13,22 +13,35 @@ import {
 import { setUserDoc } from "../../api/firebase/Doc";
 
 const Index = () => {
+  // Initialisation du hook useRouter pour gérer le routage de l'application
   const router = useRouter();
 
+  // Initialisation du hook useDispatch pour gérer l'état global de l'application
   const dispatch = useDispatch();
+
+  // Initialisation du hook useSelector pour accéder à l'état global de l'application
   const state = useSelector((state) => state);
+
+  // Initialisation du hook useSelector pour accéder à l'état global des informations utilisateur
   const stateUserInformations = useSelector((state) => state.userInformations);
+
+  // Initialisation du hook useSelector pour accéder à l'état global des éléments d'estimation
   const stateEstimationElements = useSelector(
     (state) => state.estimationElements
   );
+
+  // Initialisation d'un objet Date pour stocker la date et l'heure de l'estimation
   const date = new Date();
 
+  // Utilisation du hook useEffect pour exécuter une action au montage du composant
   useEffect(() => {
+    // Si l'élément "accommodation" n'est pas défini, l'application est redirigée vers la page d'estimation immobilière
     if (state.estimationElements.accommodation === null) {
       router.push("/estimation-immobiliere");
     }
   }, []);
 
+  // Définition d'un objet qui contient toutes les informations d'estimation, y compris l'identifiant unique généré, la date et l'heure, les informations utilisateur et les éléments d'estimation
   const allEstimaitonInformations = {
     id: createId,
     data: {
@@ -39,7 +52,9 @@ const Index = () => {
     stateEstimationElements,
   };
 
+  // Utilisation du hook useEffect pour exécuter une action lorsqu'un des éléments de l'objet stateUserInformations change
   useEffect(() => {
+    // Si toutes les informations utilisateur sont remplies, la fonction udateEstimationInformationInReducer est appelée
     if (
       stateUserInformations.firstName !== null &&
       stateUserInformations.lastName !== null &&
@@ -56,23 +71,34 @@ const Index = () => {
     stateUserInformations.mail,
   ]);
 
+  // Définition d'une fonction pour mettre à jour les informations d'estimation dans le store Redux
   const udateEstimationInformationInReducer = () => {
+    // Utilisation de la fonction setUserDoc de l'API Firebase pour enregistrer les données utilisateur
     setUserDoc(allEstimaitonInformations)
       .then(() => {
+        // Mise à jour de l'identifiant d'estimation dans le store Redux
         dispatch(updateId(allEstimaitonInformations.id));
+        // Redirection de l'utilisateur vers la page de résultat de l'estimation immobilière
         router.push("/estimation-immobiliere/resultat");
       })
       .catch((err) => {
+        // Affichage d'un message d'erreur en cas d'échec de l'enregistrement
         alert("Une erreur est survenue, veuillez essayer à nouveau.");
         console.log(err);
+        // Mise à jour de l'état global de l'application pour masquer le loader
         dispatch(handleLoader(false));
       });
   };
 
+  // Utilisation du hook useEffect pour exécuter une action au montage du composant
   useEffect(() => {
+    // Mise à jour de l'état global de l'application pour masquer le loader
     dispatch(handleLoader(false));
+    // Mise à jour de l'état global de l'application pour indiquer que l'étape courante est la dernière étape
     dispatch(updateStepOfProjectProgress("lastStep"));
   }, []);
+
+  // Rendu de la page d'estimation immobilière
 
   return (
     <LayoutEstimation>
