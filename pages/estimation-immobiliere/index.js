91a524a633b556705b82761d5efb7bb4f9eac454
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-02-16 14:22:09 +0100

    Update index.js

--- pages/estimation-immobiliere/index.js
+++ pages/estimation-immobiliere/index.js
@@ -14,12 +14,6 @@ const EstimationImmobiliere = () => {
     dispatch(updateStepOfProjectProgress(0));
   }, []);
 
-  useEffect(() => {
-    getAllEstimation()
-      .then((response) => console.log(response))
-      .catch((error) => console.log(error));
-  }, []);
-
   return (
     <LayoutEstimation>
       <div className="w-full flex mx-auto flex-col justify-center md:flex-row md:max-w-screen-2xl md:py-8">
