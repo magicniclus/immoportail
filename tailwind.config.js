76580485485663e6c6f894cafa1472b2c2aaa4a7
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-10 15:27:11 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
@@ -9,35 +9,35 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       },
       keyframes: {
         wiggle: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
         },
         animation: {
-          wiggle: 'wiggle 1s ease-in-out',
-        }
-      }
+          wiggle: "wiggle 1s ease-in-out",
+        },
+      },
     },
-    colors:{
-      "transparent": 'transparent',
-      "current": 'currentColor',
-      "purple": "#362676",
-      "purpleLight": "#665997",
-      "blue": "#1374F2",
-      "yellow": "#F2B544",
-      "orange": "#F26D3D",
-      "newGray": "#D9D9D9",
-      "grayLight": "#FAFAFA",
-      "black": colors.black,
-      "white": colors.white,
-      "gray": colors.slate,
-      "green": colors.emerald,
-      "pink": colors.fuchsia,
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      purple: "#362676",
+      purpleLight: "#665997",
+      blue: "#1374F2",
+      yellow: "#F2B544",
+      orange: "#F26D3D",
+      newGray: "#D9D9D9",
+      grayLight: "#FAFAFA",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      green: colors.emerald,
+      pink: colors.fuchsia,
     },
     extend: {},
   },
   plugins: [],
-}
+};
